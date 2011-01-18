4b815bcc0c8796d3364293addb90aa10868577d1
Flow86 <github@fd.mytrap.de>
2011-01-18 17:56:39 +0000

    Subversion Revision 6994:
    self-replacement-updater update
    
    M: /s25client/branches/s25client_0.7/release/bin/rttr.bat

--- release/bin/rttr.bat
+++ release/bin/rttr.bat
@@ -3,12 +3,7 @@
 IF EXIST RTTR\s25update.exe GOTO UPDATE
 GOTO START
 
-:UPDATE
-mkdir update
-xcopy /Y RTTR\s25update.exe update
-xcopy /Y RTTR\libcurl.dll update
-
-update\s25update.exe --stable -d %CD%
+RTTR\s25update.exe --stable -d %CD%
 
 :START
 s25client.exe
