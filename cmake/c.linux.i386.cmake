1181dd6966bdac29d58be3e3a53255525eed508e
Flow86 <github@fd.mytrap.de>
2011-04-15 18:19:47 +0000

    Subversion Revision 7170:
    fixed linux i686 cross build
    
    M: /s25client/trunk/cmake/c.linux.i386.cmake

--- cmake/c.linux.i386.cmake
+++ cmake/c.linux.i386.cmake
@@ -1,5 +1,5 @@
 ################################################################################
-### $Id: c.linux.i386.cmake 6807 2010-10-18 14:12:04Z FloSoft $
+### $Id: c.linux.i386.cmake 7170 2011-04-15 18:19:47Z FloSoft $
 ################################################################################
 
 # this one is important
@@ -7,16 +7,20 @@ SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR i686)
 
 # specify the cross compiler
-SET_CCACHE(CMAKE_C_COMPILER   i486-linux-gnu-gcc)
-SET_CCACHE(CMAKE_CXX_COMPILER i486-linux-gnu-g++)
-SET(CMAKE_RANLIB i486-linux-gnu-ranlib CACHE PATH "" FORCE)
+SET_CCACHE(CMAKE_C_COMPILER   i686-pc-linux-gnu-gcc)
+SET_CCACHE(CMAKE_CXX_COMPILER i686-pc-linux-gnu-g++)
 
-# find AR program
-FIND_PROGRAM(LINUX_AR NAMES i486-linux-gnu-ar i686-linux-gnu-ar ar DOC "path to linux's ar executable")
-SET(CMAKE_AR "${LINUX_AR}" CACHE PATH "" FORCE)
+# find ranlib program
+SET(CMAKE_RANLIB i686-pc-linux-gnu-ranlib CACHE PATH "" FORCE)
+
+# find ar program
+SET(CMAKE_AR i686-pc-linux-gnu-ar CACHE PATH "" FORCE)
 
 # set compiler flags for i686
 FORCE_ADD_FLAGS(CMAKE_C_FLAGS -malign-double)
 FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -malign-double)
+
+# set search prefix
+SET(CMAKE_PREFIX_PATH "/usr/i686-pc-linux-gnu")
 
 INCLUDE(cmake/c.linux.common.cmake)
