811c7203ee50301a3644fe88706160ec6ffbe122
MarcusSt <MarcusSt@siedler25.org>
2012-04-01 12:58:20 +0000

    Subversion Revision 7918:
    Enable debug symbols for every linux/windows. They are stripped during postinstall.
    
    M: /s25client/trunk/cmake/windows.common.cmake
    M: /s25client/trunk/cmake/linux.common.cmake

--- cmake/linux.common.cmake
+++ cmake/linux.common.cmake
@@ -1,10 +1,10 @@
 ################################################################################
-### $Id: linux.common.cmake 6807 2010-10-18 14:12:04Z FloSoft $
+### $Id: linux.common.cmake 7918 2012-04-01 12:58:20Z marcus $
 ################################################################################
 
 # set compiler flags
-ADD_FLAGS(CMAKE_C_FLAGS -ffast-math -mmmx -msse -mfpmath=sse)
-ADD_FLAGS(CMAKE_CXX_FLAGS -ffast-math -mmmx -msse -mfpmath=sse)
+FORCE_ADD_FLAGS(CMAKE_C_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
+FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
 
 # search for programs in the build host directories
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
--- cmake/windows.common.cmake
+++ cmake/windows.common.cmake
@@ -1,10 +1,10 @@
 ################################################################################
-### $Id: windows.common.cmake 6807 2010-10-18 14:12:04Z FloSoft $
+### $Id: windows.common.cmake 7918 2012-04-01 12:58:20Z marcus $
 ################################################################################
 
 # set compiler flags
-FORCE_ADD_FLAGS(CMAKE_C_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer)
-FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer)
+FORCE_ADD_FLAGS(CMAKE_C_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer -ggdb)
+FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer -ggdb)
 
 # bugfix for cygwin
 #ADD_DEFINITIONS(-D_WIN32 -D__USE_W32_SOCKETS)
