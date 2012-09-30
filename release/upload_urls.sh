47b34e664d839ef383f4cc233882eb1549d89b34
Flow86 <github@fd.mytrap.de>
2012-09-30 17:02:48 +0000

    Subversion Revision 8362:
    beautify script
    
    M: /s25client/trunk/release/upload_urls.sh

--- release/upload_urls.sh
+++ release/upload_urls.sh
@@ -23,14 +23,16 @@ if [ ! -d "$TARGET" ] ; then
 fi
 
 # create changelog
+echo "Creating Changelog ..."
 svn log $(dirname $(svn info | grep "URL" | cut -d ' ' -f 2)) > ${UPLOADFILE}changelog.txt || exit 2
 
 # upload data
+echo "Uploading Data ..."
 scp -4 ${UPLOADFILE}rapidshare.txt $UPLOADTARGET || exit 3
 scp -4 ${UPLOADFILE}changelog.txt $UPLOADTARGET || exit 4
 
 if [ ! -z "$UPLOADSCRIPT" ] ; then
-	echo "running post upload script"
+	echo "Running post upload script ..."
 	bash -c "$UPLOADSCRIPT"
 fi
 
