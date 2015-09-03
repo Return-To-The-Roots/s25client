e1fb44a454522e2ffe3604ff4dbc8c5a65d23d4a
Flamefire <Alex@grundis.de>
2015-09-04 00:20:05 +0200

    Fix for MSVC and move all defines into windows.cmake

--- cmake/windows.cmake
+++ cmake/windows.cmake
@@ -1,10 +1,31 @@
+FORCE_ADD_FLAGS(CMAKE_C_FLAGS -DNOMINMAX)
+FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -DNOMINMAX)
 
-# set compiler flags
-FORCE_ADD_FLAGS(CMAKE_C_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer -ggdb)
-FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer -ggdb)
+IF(NOT MSVC)
+	# set compiler flags
+	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer -ggdb)
+	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -malign-double -ffast-math -mmmx -msse -mfpmath=sse -fomit-frame-pointer -ggdb)
 
-# bugfix for cygwin
-#ADD_DEFINITIONS(-D_WIN32 -D__USE_W32_SOCKETS)
+	# bugfix for cygwin
+	#ADD_DEFINITIONS(-D_WIN32 -D__USE_W32_SOCKETS)
 
-FORCE_ADD_FLAGS(CMAKE_C_FLAGS -D_WIN32 -D__USE_W32_SOCKETS -DNOMINMAX)
-FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -D_WIN32 -D__USE_W32_SOCKETS -DNOMINMAX)
+	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -D_WIN32 -D__USE_W32_SOCKETS)
+	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -D_WIN32 -D__USE_W32_SOCKETS)
+ELSE(NOT MSVC)
+	# disable MSVC "use secure function" and 4250: 'class1' : inherits 'class2::member' via dominance (virtual inheritance related)
+	ADD_DEFINITIONS(-D_CRT_SECURE_NO_WARNINGS -D_SCL_SECURE_NO_WARNINGS /wd"4250")
+	# disable MSVC posix functions
+	ADD_DEFINITIONS(-D_CRT_NONSTDC_NO_DEPRECATE)
+	ADD_DEFINITIONS(-D_WINSOCK_DEPRECATED_NO_WARNINGS)
+	SET(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /Oi /GL") # systemintern functions for faster code; Optimize whole program
+	# Strip unused symbols and us COMDAT folding
+	SET(CMAKE_EXE_LINKER_FLAGS_RELEASE "${CMAKE_EXE_LINKER_FLAGS_RELEASE} /OPT:REF /OPT:ICF")
+    SET(CMAKE_EXE_LINKER_FLAGS_MINSIZEREL "${CMAKE_EXE_LINKER_FLAGS_MINSIZEREL} /OPT:REF /OPT:ICF")
+    SET(CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "${CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO} /OPT:REF /OPT:ICF") 
+	# Add optimized debugging features
+	IF (MSVC_VERSION GREATER 1800) #VS13
+		ADD_DEFINITIONS(/d2Zi+)
+	ELSEIF (NOT(MSVC_VERSION LESS 1800)) # VS12
+		ADD_DEFINITIONS(/Zo)
+	ENDIF()
+ENDIF(NOT MSVC)
