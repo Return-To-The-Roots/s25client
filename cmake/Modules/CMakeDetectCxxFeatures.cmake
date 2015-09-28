ac8ba89129de46b061086987fbc1135e7120bc7a
Flamefire <alex@grundis.de>
2015-09-28 14:29:06 +0200

    Improve override detection

--- cmake/Modules/CMakeDetectCxxFeatures.cmake
+++ cmake/Modules/CMakeDetectCxxFeatures.cmake
@@ -21,7 +21,7 @@ CHECK_CXX_SOURCE_COMPILES("
 " CXX_HAVE_CONSTEXPR
 )
 
-UNSET(CXX_HAVE_OVERRIDE CACHE)
+UNSET(CXX_OVERRIDE_OK CACHE)
 CHECK_CXX_SOURCE_COMPILES("
 	class Base{
 	public:
@@ -40,5 +40,34 @@ CHECK_CXX_SOURCE_COMPILES("
 
 		return (b.foo(2) * 2 == s.foo(2)) ? 0 : 1;
 	}
-" CXX_HAVE_OVERRIDE
+" CXX_OVERRIDE_OK FAIL_REGEX "override controls .override/final. only available with"
 )
+
+# This should fail!
+UNSET(CXX_OVERRIDE_FAIL CACHE)
+CHECK_CXX_SOURCE_COMPILES("
+	class Base{
+	public:
+		virtual int foo(int bar){ return bar; }
+	};
+
+	class Sub : public Base {
+	public:
+		virtual int foo2(int bar) override{ return 2*bar; };
+	};
+
+	int main()
+	{
+		Base b;
+		Sub s;
+
+		return (b.foo(2) * 2 == s.foo2(2)) ? 0 : 1;
+	}
+" CXX_OVERRIDE_FAIL
+)
+
+IF(CXX_OVERRIDE_OK AND NOT CXX_OVERRIDE_FAIL)
+    SET(CXX_HAVE_OVERRIDE YES CACHE INTERNAL "")
+ELSE()
+    SET(CXX_HAVE_OVERRIDE NO CACHE INTERNAL "")
+ENDIF()
