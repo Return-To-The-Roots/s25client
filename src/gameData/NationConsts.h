4788125147b09ad174714c00d4cc1fdfdd76d502
Flamefire <Alex@grundis.de>
2015-10-26 19:22:15 +0100

    Add header guard

--- src/gameData/NationConsts.h
+++ src/gameData/NationConsts.h
@@ -1,4 +1,24 @@
+// Copyright (c) 2005 - 2015 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Return To The Roots.
+//
+// Return To The Roots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Return To The Roots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+
 #pragma once
+
+#ifndef NationConsts_h__
+#define NationConsts_h__
 
 #include <boost/array.hpp>
 #include <string>
@@ -28,3 +48,5 @@ const boost::array<unsigned char, NAT_COUNT> NATION_RTTR_TO_S2 =
     1,
     0 /* Babylonians get the roman figures where no others are used */
 }};
+
+#endif // NationConsts_h__
