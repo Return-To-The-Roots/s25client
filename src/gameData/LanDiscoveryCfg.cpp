5857975f78b706a92d38470049f0b02542ebfb7e
Alexander Grund <Alex@grundis.de>
2015-12-17 20:13:17 +0100

    Fix spelling of file

--- src/gameData/LanDiscoveryCfg.cpp
+++ src/gameData/LanDiscoveryCfg.cpp
@@ -16,7 +16,7 @@
 // along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
 
 #include "defines.h"
-#include "LANDiscoveryCfg.h"
+#include "LanDiscoveryCfg.h"
 
 static LANDiscoveryBase::Config makeDiscoveryConfig()
 {
