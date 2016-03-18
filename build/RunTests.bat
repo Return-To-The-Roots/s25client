d590145fb6f130bdb48aa30acd720d4ca7c500f7
Flamefire <Alex@grundis.de>
2016-03-18 14:41:11 +0100

    Add test case for lua functions (windows only)

---
+++ build/RunTests.bat
@@ -0,0 +1,33 @@
+echo off
+
+set numVersions=0
+if exist src\Debug\s25client.exe set /a numVersions=numVersions+1 & set version=1
+if exist src\ReWithDebInfo\s25client.exe set /a numVersions=numVersions+1 & set version=2
+if exist src\Release\s25client.exe set /a numVersions=numVersions+1 & set version=3
+
+if %numVersions%==1 goto EXECUTE
+
+echo "Which version shall I execute?"
+echo "1 -> Debug"
+echo "2 -> ReWithDebInfo (default)"
+echo "3 -> Release"
+set version=2
+set /p version="Type number and enter"
+
+if %version%==1 (
+  set version=Debug
+) else (
+  if %version%==2 (
+    set version=RelWithDebInfo
+  ) else (
+    set version=Release
+  )
+)
+
+:EXECUTE
+
+set cmd=src\%version%\s25client.exe --test ..\tests\maps\LuaFunctions.SWD
+echo %cmd%
+%cmd%
+pause
+
