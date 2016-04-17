0f5b23ca83c030c5c580ad7676773a769a0edcf1
Flamefire <Alex@grundis.de>
2016-04-17 21:22:59 +0200

    Fix invalid chars in Trade Addon

--- src/addons/AddonTrade.h
+++ src/addons/AddonTrade.h
@@ -17,13 +17,9 @@
 #ifndef ADDONTRADE_H_INCLUDED
 #define ADDONTRADE_H_INCLUDED
 
-
 #include "Addons.h"
 #include "mygettext/src/mygettext.h"
 
-///////////////////////////S/**
- *  Addon for a Charburner
- */
 class AddonTrade : public AddonBool
 {
     public:
