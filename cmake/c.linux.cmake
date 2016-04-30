ad1fd9ade5e279c0826fde27257c50978f56204b
Alexander Grund <Flamefire@users.noreply.github.com>
2016-04-30 21:52:20 +0200

    Add -mno-align-double in cross linux (32Bit) for #443

--- cmake/c.linux.cmake
+++ cmake/c.linux.cmake
@@ -1,5 +1,5 @@
 SET(Boost_COMPILER "-gcc44")
 
 # set compiler flags
-FORCE_ADD_FLAGS(CMAKE_C_FLAGS -mtune=generic)
-FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -mtune=generic)
+FORCE_ADD_FLAGS(CMAKE_C_FLAGS -mtune=generic -mno-align-double)
+FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -mtune=generic -mno-align-double)
