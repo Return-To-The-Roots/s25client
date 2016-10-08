7d76fc707593b17ddaa582e5d97c89bafab73920
Flamefire <git@grundis.de>
2016-10-08 13:00:22 +0200

    Correct RTTR folder copy and some minor issues (cleanup, ignore...)

--- build/cleanup.sh
+++ build/cleanup.sh
@@ -8,8 +8,8 @@ if (test "${TOP_SRCDIR}err" = "err") || (test "${TOP_SRCDIR}" = ".") ; then
 fi
 
 echo "Cleaning up $PWD"
-rm -vf CMakeCache.txt cmake_install.cmake Makefile install_manifest.txt
-rm -vrf CMakeFiles Testing build_paths.h build_version.h build_version_defines.h postinstall.sh CTestTestfile.cmake
+rm -vf CMakeCache.txt cmake_install.cmake Makefile install_manifest.txt prepareRelease.sh
+rm -vrf CMakeFiles Testing bugle build_paths.h build_version.h build_version_defines.h postinstall.sh CTestTestfile.cmake
 rm -vf *.bak *~ .DS_Store
 
 # Unterverzeichnisse entfernen
