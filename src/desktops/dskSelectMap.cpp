0b90fa7fd36197ec7af89c32c712b09be4bb9259
Timm Hoffmeister <th@re-flekt.com>
2016-11-16 22:14:50 +0100

    Remove dependency to boost which is not working on some CI jobs

--- src/desktops/dskSelectMap.cpp
+++ src/desktops/dskSelectMap.cpp
@@ -48,7 +48,6 @@
 #include "libutil/src/ucString.h"
 #include <boost/foreach.hpp>
 #include <boost/filesystem.hpp>
-#include <boost/move/unique_ptr.hpp>
 
 /** @class dskSelectMap
  *
@@ -252,7 +251,7 @@ void dskSelectMap::StartRandomMap()
     settings.type = 0x00;
 
     // create new map generator
-    boost::movelib::unique_ptr<MapGenerator> generator(new MapGenerator);
+    MapGenerator* generator = new MapGenerator();
     
     // setup filepath for the random map
     map_path = GetFilePath(FILE_PATHS[48]);
@@ -260,6 +259,9 @@ void dskSelectMap::StartRandomMap()
 
     // create a random map and save filepath
     generator->Create(map_path, Style::Random, settings);
+    
+    // memory cleanup
+    delete generator; generator = NULL;
     
     // close & cleanup "please wait" window
     WINDOWMANAGER.Close(waitWindow);
