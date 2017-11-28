5bbdf01d2e25417f8a286c9ffc7ecb0043ee4035
Flamefire <git@grundis.de>
2017-11-28 17:08:34 +0100

    Fix header

--- src/ingameWindows/iwAddons.h
+++ src/ingameWindows/iwAddons.h
@@ -1,19 +1,19 @@
-// Copyright (c) 2005-2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
 //
-// This file is part of Siedler II.5 RTTR.
+// This file is part of Return To The Roots.
 //
-// Siedler II.5 RTTR is free software: you can redistribute it and/or modify
+// Return To The Roots is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// Siedler II.5 RTTR is distributed in the hope that it will be useful,
+// Return To The Roots is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Siedler II.5 RTTR. If not, see <http://www.gnu.org/licenses/>.
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
 #ifndef iwADDONS_H_INCLUDED
 #define iwADDONS_H_INCLUDED
 
