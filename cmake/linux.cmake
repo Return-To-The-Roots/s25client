c8e6902ee4bc53129e3fb4b7cbac70fbad084d36
daviel <davidkrawiec@posteo.net>
2018-03-15 15:18:25 +0000

    added build support for armv7l (raspberry pi 3)

--- cmake/linux.cmake
+++ cmake/linux.cmake
@@ -11,6 +11,11 @@ endif(CCACHE_FOUND)
 
 # set compiler flags
 IF(RTTR_ENABLE_OPTIMIZATIONS)
-	FORCE_ADD_FLAGS(CMAKE_C_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
-	FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
+	IF (PLATFORM_ARCH STREQUAL "armv7l")
+		FORCE_ADD_FLAGS(CMAKE_C_FLAGS -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8 -mneon-for-64bits -mtune=cortex-a53)
+		FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8 -mneon-for-64bits -mtune=cortex-a53)
+	ELSE ()
+		FORCE_ADD_FLAGS(CMAKE_C_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
+		FORCE_ADD_FLAGS(CMAKE_CXX_FLAGS -ffast-math -mmmx -msse -mfpmath=sse -ggdb)
+	ENDIF (PLATFORM_ARCH STREQUAL "armv7l")
 ENDIF(RTTR_ENABLE_OPTIMIZATIONS)
