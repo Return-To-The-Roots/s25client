9d9bb9287a9d06db10fca8256a4f54dabc14f9b9
Shawn8901 <shawn8901@googlemail.com>
2018-04-23 17:35:20 +0200

    Correction for wrong documentation

--- src/world/World.h
+++ src/world/World.h
@@ -120,7 +120,7 @@ public:
     void ChangeAltitude(const MapPoint pt, const unsigned char altitude);
 
     /// Checks if the point completely belongs to a player (if false but point itself belongs to player then it is a border)
-    /// if owner is != '0' it checks if the points specific ownership
+    /// if owner is != 0 it checks if the points specific ownership
     bool IsPlayerTerritory(const MapPoint pt, const unsigned char owner = 0) const;
 
     /// Return the BQ for the given player at the point (including ownership constraints)
