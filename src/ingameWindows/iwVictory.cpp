3ab1295cbd5ac2acdcb36656c9a47f8862a8e59b
Flamefire <git@grundis.de>
2018-11-19 20:26:07 +0100

    Fix color of continue bt and add test

--- src/ingameWindows/iwVictory.cpp
+++ src/ingameWindows/iwVictory.cpp
@@ -52,7 +52,7 @@ iwVictory::iwVictory(const std::vector<std::string>& winnerNames)
         SetIwSize(iwSize);
     names->SetPos(DrawPoint((GetSize().x - txtBounds.getSize().x) / 2, names->GetPos().y));
     int btPosY = std::max(170, txtBounds.bottom + 20);
-    AddTextButton(ID_CONTINUE, DrawPoint(50, btPosY), Extent(140, 20), TC_GREEN2, _("Continue game"), NormalFont);
+    AddTextButton(ID_CONTINUE, DrawPoint(50, btPosY), Extent(140, 20), TC_GREY, _("Continue game"), NormalFont);
     AddTextButton(ID_END_GAME, DrawPoint(50, btPosY + 25), Extent(140, 20), TC_RED1, _("End game"), NormalFont);
 }
 
