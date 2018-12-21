61a3050a178d8dafa9082e003140a64937c00b0f
Flamefire <git@grundis.de>
2018-12-21 12:22:14 +0100

    Move CI build commands into scripts
    Allows better error handling (set -e)
    Makes tests easier
    Code reuse (e.g. installCMake)

---
+++ tools/ci/installCMake.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CMAKE_VERSION="${1:?Missing CMake version}"
+INSTALL_DIR="${2:?Missing install dir}"
+CONFIGURE_PREFIX_DIR="${3:?Missing directory prefix where to build CMake}" # Where to store downloaded and build files
+
+export PATH="${INSTALL_DIR}/bin:${PATH}"
+
+curVersion="$(cmake --version 2>/dev/null || true)"
+
+if echo "${curVersion}" | grep -q "cmake version ${CMAKE_VERSION}"; then
+    echo "CMake ${CMAKE_VERSION} already installed: `which cmake`"
+    exit 0
+fi
+
+mkdir -p "${CONFIGURE_PREFIX_DIR}" && cd "${CONFIGURE_PREFIX_DIR}"
+
+# Encode version into directory to find it again
+BUILD_DIR="cmake-${CMAKE_VERSION}"
+if [ ! -f "${BUILD_DIR}/bootstrap" ]; then
+    CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz"
+    wget "${CMAKE_URL}" -qO- | tar xz
+    if [ ! -f "${BUILD_DIR}/bootstrap" ]; then
+        echo "Download failed or files invalid" >&2
+        exit 1
+    fi
+fi
+
+cd "${BUILD_DIR}"
+# Build quietely preferably with cmake (if it exists) or fallback to bootstrap
+cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" >/dev/null || \
+    ./bootstrap --prefix="${INSTALL_DIR}" --parallel=$(nproc) >/dev/null
+
+make install -j$(nproc) >/dev/null
---
+++ tools/ci/travisBuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Information
+cmake --version
+$CXX --version
+
+INSTALL_DIR="${TRAVIS_BUILD_DIR}/installed"
+rm -rf "${INSTALL_DIR}"
+mkdir -p build && cd build
+cmake .. -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+    -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
+    -DRTTR_ENABLE_WERROR=ON -DRTTR_EDITOR_ADMINMODE=ON \
+    -G "Unix Makefiles" ${ADDITIONAL_CMAKE_FLAGS}
+# Travis uses 2 cores
+if [[ "${BUILD_TYPE}" == "Release" ]]; then
+    make -j2 install
+else
+    make -j2
+fi
+
+# Execute tests
+export BOOST_TEST_CATCH_SYSTEM_ERRORS="no"
+export RTTR_DISABLE_ASSERT_BREAKPOINT=1
+ctest --output-on-failure=1 -j2
