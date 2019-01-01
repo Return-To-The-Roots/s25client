43b1862aab50ee097add89f6bd128d0da6a4a729
Flow86 <github@fd.mytrap.de>
2019-01-01 18:41:24 +0100

    FIX: fix audio loading. it seems they used multiple rpaths.
    We should really print out whats going wrong if we can't load a driver in DriverWrapper.cpp
    dlerror or FormatMessage/GetLastError on windows.
    
    Thats the only way I've found out why the rpath was still wrong (i.e missing an additional entry)

--- cmake/darwin.cmake
+++ cmake/darwin.cmake
@@ -49,6 +49,6 @@ set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -fexceptions")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -fexceptions")
 
 # add rpath to linker flags
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -rpath @executable_path -rpath @executable_path/..")
-set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -rpath @executable_path -rpath @executable_path/..")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -rpath @executable_path -rpath @executable_path/..")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -rpath @executable_path -rpath @executable_path/.. -rpath @executable_path/../Frameworks")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -rpath @executable_path -rpath @executable_path/.. -rpath @executable_path/../Frameworks")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -rpath @executable_path -rpath @executable_path/.. -rpath @executable_path/../Frameworks")
