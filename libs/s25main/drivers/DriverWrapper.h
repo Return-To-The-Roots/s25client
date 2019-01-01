0439880b4ce9021ace6dd4b2c1b563032e25c0cb
Flamefire <git@grundis.de>
2019-01-01 22:32:11 +0100

    Add missing define

--- libs/s25main/drivers/DriverWrapper.h
+++ libs/s25main/drivers/DriverWrapper.h
@@ -23,7 +23,14 @@
 
 #include <boost/filesystem/path.hpp>
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
+#else
+#ifndef HINSTANCE
+#define HINSTANCE void*
+#endif
 #endif
 
 #include <string>
