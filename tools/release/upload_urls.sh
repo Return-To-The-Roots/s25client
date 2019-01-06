93ad741a729d2c8410e1967c73cacdaee779fc41
Flow86 <github@fd.mytrap.de>
2019-01-06 15:47:33 +0100

    FIX: upload urls fails because paths outside of docker container vs inside container

--- tools/release/upload_urls.sh
+++ tools/release/upload_urls.sh
@@ -24,17 +24,17 @@ fi
 
 # create changelog
 echo "Creating Changelog ..."
-git log --submodule --no-max-parents | sed -r "s#commit (.*)#commit \1\nRepository: $(git remote -v | head -n 1 | egrep -o "([^/]*)\.git" | sed s/.git//g)#g" > ${UPLOADFILE}changelog.txt || exit 2
-git submodule foreach 'git log --submodule --no-max-parents | sed -r "s#commit (.*)#commit \1\nRepository: $(git remote -v | head -n 1 | egrep -o "([^/]*)\.git" | sed s/.git//g)#g"' >> ${UPLOADFILE}changelog.txt || exit 2
+git log --submodule --no-max-parents | sed -r "s#commit (.*)#commit \1\nRepository: $(git remote -v | head -n 1 | egrep -o "([^/]*)\.git" | sed s/.git//g)#g" > ${UPLOADFILE2}changelog.txt || exit 2
+git submodule foreach 'git log --submodule --no-max-parents | sed -r "s#commit (.*)#commit \1\nRepository: $(git remote -v | head -n 1 | egrep -o "([^/]*)\.git" | sed s/.git//g)#g"' >> ${UPLOADFILE2}changelog.txt || exit 2
 
 
-cp ${UPLOADFILE}changelog.txt changelog-$TYPE.txt
-cp ${UPLOADFILE}rapidshare.txt rapidshare-$TYPE.txt
+cp ${UPLOADFILE2}changelog.txt changelog-$TYPE.txt
+cp ${UPLOADFILE2}rapidshare.txt rapidshare-$TYPE.txt
 
 # upload data
 echo "Uploading Data ..."
-scp -4 ${UPLOADFILE}rapidshare.txt $UPLOADTARGET$UPLOADTO || exit 3
-scp -4 ${UPLOADFILE}changelog.txt $UPLOADTARGET$UPLOADTO || exit 4
+scp -4 ${UPLOADFILE2}rapidshare.txt $UPLOADTARGET$UPLOADTO || exit 3
+scp -4 ${UPLOADFILE2}changelog.txt $UPLOADTARGET$UPLOADTO || exit 4
 
 if [ ! -z "$UPLOADSCRIPT" ] ; then
 	echo "Running post upload script ..."
