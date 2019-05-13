ce9c8043d6c93f525723712f96a5f345a7a0398f
Flamefire <git@grundis.de>
2019-05-13 13:01:22 +0200

    Exclude all external coverage but include tests

--- tools/ci/uploadCoverageData.sh
+++ tools/ci/uploadCoverageData.sh
@@ -3,16 +3,4 @@
 set -euo pipefail
 
 external/libutil/tools/ci/uploadCoverageData.sh \
-    '*/external/full-contrib-msvc/*' \
-    '*/external/glad/*' \
-    '*/external/kaguya/*' \
-    '*/external/languages/*' \
-    '*/external/libendian/*' \
-    '*/external/liblobby/*' \
-    '*/external/lua/*' \
-    '*/external/macos/*' \
-    '*/external/s25edit/*' \
-    '*/external/s25update/*' \
-    '*/external/s-c/*' \
-    '*/external/turtle/*' \
-    '*/tests/*'
+    '*/external/*'
