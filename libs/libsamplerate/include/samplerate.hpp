0519287ab2e1e9531e9dbb7ab9399c6969083788
Flamefire <Flamefire@users.noreply.github.com>
2019-07-27 16:20:39 +0200

    Add libsamplerate and C++ wrapper

---
+++ libs/libsamplerate/include/samplerate.hpp
@@ -0,0 +1,185 @@
+// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Return To The Roots.
+//
+// Return To The Roots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Return To The Roots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef samplerate_hpp__
+#define samplerate_hpp__
+
+#include <functional>
+#include <samplerate.h>
+#include <stdexcept>
+#include <utility>
+
+namespace samplerate {
+enum class Converter
+{
+    SincBestQuality = SRC_SINC_BEST_QUALITY,
+    SincMediumQuality = SRC_SINC_MEDIUM_QUALITY,
+    SincFastest = SRC_SINC_FASTEST,
+    ZeroOrder = SRC_ZERO_ORDER_HOLD,
+    Linear = SRC_LINEAR
+};
+
+inline const char* getName(Converter type)
+{
+    return src_get_name(static_cast<int>(type));
+}
+inline const char* getDescription(Converter type)
+{
+    return src_get_description(static_cast<int>(type));
+}
+
+namespace detail {
+    [[noreturn]] inline void throwError(int errorCode) { throw std::runtime_error(src_strerror(errorCode)); }
+    inline void throwOnError(int errorCode)
+    {
+        if(errorCode)
+            throw std::runtime_error(src_strerror(errorCode));
+    }
+
+    class State
+    {
+        SRC_STATE* state_;
+
+    public:
+        explicit State(SRC_STATE* state) : state_(state) {}
+        ~State() { src_delete(state_); }
+        State(const State& other) : state_(nullptr)
+        {
+            if(other.state_)
+            {
+                int error;
+                state_ = src_clone(other.state_, &error);
+                if(!state_)
+                    throwOnError(error);
+            }
+        }
+        State(State&& other) noexcept : state_(other.state_) { other.state_ = nullptr; }
+        State& operator=(State other) noexcept
+        {
+            std::swap(state_, other.state_);
+            return *this;
+        }
+        operator SRC_STATE*() { return state_; }
+    };
+
+    class StateBase
+    {
+    protected:
+        State state_;
+        explicit StateBase(SRC_STATE* state) : state_(state) {}
+
+    public:
+        void reset() { throwOnError(src_reset(state_)); }
+        void setRatio(double newRatio) { throwOnError(src_set_ratio(state_, newRatio)); }
+        int getChannels()
+        {
+            int result = src_get_channels(state_);
+            if(result < 0)
+                throwError(-result);
+            return result;
+        }
+        /// Get internal state
+        SRC_STATE* getState() { return state_; }
+    };
+} // namespace detail
+
+struct Data
+{
+    SRC_DATA data;
+    Data(const float* in, size_t input_frames, float* out, size_t output_frames, double src_ratio, bool end_of_input = true) : data{}
+    {
+        data.data_in = in;
+        data.data_out = out;
+        data.input_frames = static_cast<long>(input_frames);
+        data.output_frames = static_cast<long>(output_frames);
+        data.src_ratio = src_ratio;
+        data.end_of_input = end_of_input;
+    }
+    operator SRC_DATA*() { return &data; }
+    long input_frames_used() const { return data.input_frames_used; }
+    long output_frames_gen() const { return data.output_frames_gen; }
+};
+
+struct Result
+{
+    long input_frames_used, output_frames_gen;
+};
+
+inline Result simple(Data& data, Converter converter, int channels)
+{
+    detail::throwOnError(src_simple(&data.data, static_cast<int>(converter), channels));
+    return {data.input_frames_used(), data.output_frames_gen()};
+}
+
+inline Result simple(Data&& data, Converter converter, int channels)
+{
+    return simple(static_cast<Data&>(data), converter, channels);
+}
+
+class State : public detail::StateBase
+{
+    static SRC_STATE* createOrThrow(Converter converter, int channels)
+    {
+        int error;
+        auto* state = src_new(static_cast<int>(converter), channels, &error);
+        if(!state)
+            detail::throwError(error);
+        return state;
+    }
+
+public:
+    State(Converter converter, int channels) : StateBase(createOrThrow(converter, channels)) {}
+    void process(SRC_DATA& data) { detail::throwOnError(src_process(state_, &data)); }
+    Result process(Data& data)
+    {
+        process(data.data);
+        return {data.input_frames_used(), data.output_frames_gen()};
+    }
+    Result process(Data&& data) { return process(static_cast<Data&>(data)); }
+};
+
+class StateCallback : public detail::StateBase
+{
+    static SRC_STATE* createOrThrow(Converter converter, int channels, src_callback_t func, void* data)
+    {
+        int error;
+        auto* state = src_callback_new(func, static_cast<int>(converter), channels, &error, data);
+        if(!state)
+            detail::throwError(error);
+        return state;
+    }
+    static long callback(void* cb_data, float** data) { return static_cast<long>(static_cast<StateCallback*>(cb_data)->callback_(*data)); }
+    std::function<size_t(float*&)> callback_;
+
+public:
+    StateCallback(Converter converter, int channels, src_callback_t func, void* data)
+        : StateBase(createOrThrow(converter, channels, func, data))
+    {}
+    StateCallback(Converter converter, int channels, std::function<size_t(float*&)> func)
+        : StateBase(createOrThrow(converter, channels, func ? StateCallback::callback : nullptr, this)), callback_(std::move(func))
+    {}
+    long read(double src_ratio, size_t frames, float* data)
+    {
+        const long framesGenerated = src_callback_read(state_, src_ratio, static_cast<long>(frames), data);
+        if(framesGenerated == 0)
+            detail::throwOnError(src_error(state_));
+        return framesGenerated;
+    }
+};
+} // namespace samplerate
+
+#endif // samplerate_hpp__
---
+++ tests/libsamplerate_cpp/testSamplerate.cpp
@@ -0,0 +1,136 @@
+// Copyright (c) 2016 - 2017 Settlers Freaks (sf-team at siedler25.org)
+//
+// This file is part of Return To The Roots.
+//
+// Return To The Roots is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Return To The Roots is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+
+#define _USE_MATH_DEFINES
+
+#include <boost/test/unit_test.hpp>
+#include <algorithm>
+#include <cmath>
+#include <samplerate.hpp>
+#include <vector>
+
+static int test_data = 42;
+
+long callback(void* cb_data, float** data)
+{
+    BOOST_TEST(cb_data == &test_data);
+    *data = nullptr;
+    return test_data;
+}
+
+BOOST_AUTO_TEST_CASE(InvalidCtorParamsThrow)
+{
+    BOOST_CHECK_THROW(samplerate::State(samplerate::Converter::Linear, -1), std::runtime_error);
+    BOOST_CHECK_THROW(samplerate::StateCallback(samplerate::Converter::Linear, -1, callback, &test_data), std::runtime_error);
+    BOOST_CHECK_THROW(samplerate::StateCallback(samplerate::Converter::Linear, 1, nullptr, nullptr), std::runtime_error);
+    BOOST_CHECK_THROW(samplerate::StateCallback(samplerate::Converter::Linear, 1, nullptr), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(CtorAndBaseFuncsWork)
+{
+    samplerate::State s1(samplerate::Converter::Linear, 10);
+    BOOST_TEST_REQUIRE(s1.getState());
+    BOOST_TEST(s1.getChannels() == 10);
+    s1.reset();                   // No exceptions thrown
+    s1.setRatio(44100. / 22050.); // No exceptions thrown
+}
+
+BOOST_AUTO_TEST_CASE(CopyClones)
+{
+    samplerate::State s1(samplerate::Converter::Linear, 1);
+    BOOST_TEST(s1.getState());
+    samplerate::State s2 = s1;
+    BOOST_TEST(s2.getState());
+    BOOST_TEST(s1.getState() != s2.getState());
+    samplerate::State s3{s1};
+    BOOST_TEST(s3.getState());
+    BOOST_TEST(s1.getState() != s3.getState());
+}
+
+BOOST_AUTO_TEST_CASE(MoveDoesNotCopy)
+{
+    samplerate::State s1(samplerate::Converter::Linear, 1);
+    BOOST_TEST(s1.getState());
+    const auto* s1State = s1.getState();
+    samplerate::State s2 = std::move(s1);
+    BOOST_TEST(s2.getState());
+    BOOST_TEST(s1State == s2.getState());
+    samplerate::State s3{std::move(s2)};
+    BOOST_TEST(s3.getState());
+    BOOST_TEST(s1State == s3.getState());
+}
+
+BOOST_AUTO_TEST_CASE(SimpleCallback)
+{
+    samplerate::StateCallback state(samplerate::Converter::Linear, 1, callback, &test_data);
+    test_data = 0;
+    std::vector<float> out(10);
+    const auto written = state.read(2, out.size(), out.data());
+    BOOST_TEST(written == 0);
+}
+
+BOOST_AUTO_TEST_CASE(ConversionWorks)
+{
+    std::vector<float> input(2048);
+    std::vector<float> output(input.size() * 2), output2(output.size());
+    {
+        int i = 0;
+        std::generate(input.begin(), input.end(), [&i]() mutable { return static_cast<float>(std::sin(i++ * 0.01 * 2 * M_PI)); });
+    }
+    SRC_DATA data{};
+    data.data_in = input.data();
+    data.data_out = output.data();
+    data.input_frames = static_cast<long>(input.size());
+    data.output_frames = static_cast<long>(output.size());
+    data.end_of_input = true;
+    data.src_ratio = 2;
+    BOOST_TEST_REQUIRE(src_simple(&data, SRC_LINEAR, 1) == 0);
+
+    const auto resultSimple =
+      samplerate::simple(samplerate::Data(input.data(), input.size(), output2.data(), output2.size(), 2), samplerate::Converter::Linear, 1);
+    BOOST_TEST(output == output2, boost::test_tools::per_element());
+    BOOST_TEST(resultSimple.input_frames_used == data.input_frames_used);
+    BOOST_TEST(resultSimple.output_frames_gen == data.output_frames_gen);
+
+    std::fill(output2.begin(), output2.end(), 0.f);
+    samplerate::State state(samplerate::Converter::Linear, 1);
+    const auto resultState = state.process(samplerate::Data(input.data(), input.size(), output2.data(), output2.size(), 2));
+    BOOST_TEST(output == output2, boost::test_tools::per_element());
+    BOOST_TEST(resultState.input_frames_used == data.input_frames_used);
+    BOOST_TEST(resultState.output_frames_gen == data.output_frames_gen);
+
+    std::fill(output2.begin(), output2.end(), 0.f);
+    size_t curInputPos = 0;
+    samplerate::StateCallback cbState(samplerate::Converter::Linear, 1, [&](float*& data) {
+        data = &input[curInputPos];
+        const auto size = std::min<size_t>(200u, input.size() - curInputPos);
+        curInputPos += size;
+        return size;
+    });
+    long curPos = 0;
+    while(true)
+    {
+        const auto written =
+          cbState.read(2, std::min<size_t>(500, output2.size() - curPos), curPos == output2.size() ? nullptr : &output2[curPos]);
+        if(written == 0)
+            break;
+        curPos += written;
+    }
+    BOOST_TEST(output == output2, boost::test_tools::per_element());
+    BOOST_TEST(curInputPos == data.input_frames_used);
+    BOOST_TEST(curPos == data.output_frames_gen);
+}
