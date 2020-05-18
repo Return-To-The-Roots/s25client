f120ee201319840ec12e996cc78d692fefbc5c60
Flow86 <656249+Flow86@users.noreply.github.com>
2020-05-18 18:01:12 +0200

    only run tests on native architecture

--- tools/ci/jenkins/build.sh
+++ tools/ci/jenkins/build.sh
@@ -64,14 +64,16 @@ cmake \
 
 make -j4 package
 
-export RTTR_DISABLE_ASSERT_BREAKPOINT=1
-export UBSAN_OPTIONS=print_stacktrace=1
-export AUDIODEV=null                    # Avoid errors like: ALSA lib confmisc.c:768:(parse_card) cannot find card '0'
-export SDL_VIDEODRIVER=dummy            # Avoid crash on travis
+if [ "$(uname -s | tr "[:upper:]" "[:lower:]").$(uname -m)" == "$architecture" ] ; then
+    export RTTR_DISABLE_ASSERT_BREAKPOINT=1
+    export UBSAN_OPTIONS=print_stacktrace=1
+    export AUDIODEV=null                    # Avoid errors like: ALSA lib confmisc.c:768:(parse_card) cannot find card '0'
+    export SDL_VIDEODRIVER=dummy            # Avoid crash on travis
 
-if ! ctest --output-on-failure -j2; then
-    echo "error: tests failed. re-running failed tests with extra verbosity now" >&2
-    ctest --output-on-failure --rerun-failed --extra-verbose
+    if ! ctest --output-on-failure -j2; then
+        echo "error: tests failed. re-running failed tests with extra verbosity now" >&2
+        ctest --output-on-failure --rerun-failed --extra-verbose
+    fi
 fi
 
 files=$(find . -maxdepth 1 -name '*.zip' -o -name '*.tar.bz2')
