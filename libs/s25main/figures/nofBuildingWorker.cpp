Return-To-The-Roots/s25client

commit f3257cfca78d8c9ce589726c7f87ade6a3592276

Introduce getBobId for job consts
This is the code part for allowing link override files for nation images
Job data is split into 3 arrays: Names, (game) data, and sprite data, however a single array is used to initialize those 3 to avoid repeated comments to describe each job
The sprite data uses a struct with private data as the ID into jobs.bob (can be) nation dependend so the function must be used
The remaining changes adjust all usages so the correct images are always used

libs/s25main/figures/nofBuildingWorker.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2020 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
 //
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
 
-#include "rttrDefines.h" // IWYU pragma: keep
 #include "nofBuildingWorker.h"
 #include "EventManager.h"
 #include "GamePlayer.h"
@@ -85,12 +84,9 @@ void nofBuildingWorker::Draw(DrawPoint drawPt)
     switch(state)
     {
         case STATE_FIGUREWORK:
-
         case STATE_HUNTER_CHASING:
         case STATE_HUNTER_WALKINGTOCADAVER:
-        case STATE_HUNTER_FINDINGSHOOTINGPOINT: { DrawWalking(drawPt);
-        }
-        break;
+        case STATE_HUNTER_FINDINGSHOOTINGPOINT: DrawWalking(drawPt); break;
         case STATE_WORK:
         case STATE_HUNTER_SHOOTING:
         case STATE_HUNTER_EVISCERATING:
@@ -321,7 +317,7 @@ void nofBuildingWorker::DrawWalkingWithWare(DrawPoint drawPt)
     unsigned short id = GetCarryID();
     // >=100 -> carrier.bob else jobs.bob!
     if(id >= 100)
-        DrawWalking(drawPt, LOADER.GetBobN("carrier"), id - 100, JOB_CONSTS[job_].fat);
+        DrawWalkingBobCarrier(drawPt, id - 100, JOB_SPRITE_CONSTS[job_].isFat());
     else
-        DrawWalking(drawPt, LOADER.GetBobN("jobs"), id, JOB_CONSTS[job_].fat);
+        DrawWalkingBobJobs(drawPt, job_);
 }

libs/s25main/GameObject.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2020 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
 //
@@ -20,6 +20,7 @@
 
 #pragma once
 
+#include "commonDefines.h"
 #include "gameTypes/GO_Type.h"
 #include <string>
 

libs/s25main/Loader.cpp
@@ -593,32 +593,14 @@ void Loader::fillCaches()
                         id = 0;
                     } else
                     {
-                        id = JOB_CONSTS[job].jobs_bob_id;
-                        fat = JOB_CONSTS[job].fat;
-
-                        if((job == JOB_SCOUT) || ((job >= JOB_PRIVATE) && (job <= JOB_GENERAL)))
-                        {
-                            if(nation < NUM_NATIVE_NATS)
-                            {
-                                id += NATION_RTTR_TO_S2[nation] * 6;
-                            } else if(nation == NAT_BABYLONIANS) //-V547
-                            {
-                                id += NATION_RTTR_TO_S2[NAT_ROMANS] * 6;
-                                /* TODO: change this once we have own job pictures for babylonians
-                                                                //Offsets to std::make_unique<job imgs>()
-                                                                overlayOffset = (job == JOB_SCOUT) ? 1740 : 1655;
-
-                                                                //8 Frames * 6 Directions * 6 Types
-                                                                overlayOffset += (nation - NUM_NATIVE_NATS) * (8 * 6 * 6);
-                                */
-                            } else
-                                throw std::runtime_error("Wrong nation");
-                        }
+                        id = JOB_SPRITE_CONSTS[job].getBobId(Nation(nation));
+                        fat = JOB_SPRITE_CONSTS[job].isFat();
                     }
 
                     unsigned good = id * 96 + ani_step * 12 + ((dir + 3) % 6) + fat * 6;
                     unsigned body = fat * 48 + ((dir + 3) % 6) * 8 + ani_step;
 
+                    RTTR_Assert(good < bob_jobs->getNumItems());
                     if(bob_jobs->getLink(good) == 92)
                     {
                         good -= fat * 6;

libs/s25main/desktops/dskCredits.cpp
@@ -265,12 +265,7 @@ void dskCredits::DrawBobs()
             b.hasWare = true;
         } else
         {
-            if(job == JOB_SCOUT)
-                job = 35 + NATION_RTTR_TO_S2[rand() % 4] * 6;
-            else if(job >= JOB_PRIVATE && job <= JOB_GENERAL)
-                job = 30 + NATION_RTTR_TO_S2[rand() % 4] * 6 + job - JOB_PRIVATE;
-            else
-                job = JOB_CONSTS[job].jobs_bob_id;
+            job = JOB_SPRITE_CONSTS[job].getBobId(Nation(rand() % NUM_NATS));
             b.hasWare = false;
         }
 

libs/s25main/figures/noFigure.cpp
@@ -1,5 +1,4 @@
-//
-// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2020 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
 //
@@ -16,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
 
-#include "rttrDefines.h" // IWYU pragma: keep
 #include "figures/noFigure.h"
 #include "EventManager.h"
 #include "FindWhConditions.h"
@@ -752,47 +750,42 @@ void noFigure::CorrectSplitData(const RoadSegment* const rs2)
 /// Wird aufgerufen, wenn die Straße unter der Figur geteilt wurde (für abgeleitete Klassen)
 void noFigure::CorrectSplitData_Derived() {}
 
-void noFigure::DrawWalkingBobCarrier(DrawPoint drawPt, unsigned ware, bool fat)
+unsigned noFigure::CalcWalkAnimationFrame() const
 {
-    // Wenn wir warten auf ein freies Plätzchen, müssen wir den stehend zeichnen!
-    unsigned ani_step = waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
+    // If we are waiting for a free node use the 2nd frame, else interpolate
+    return waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
+}
 
-    // Wenn man wartet, stehend zeichnen, es sei denn man wartet mittem auf dem Weg!
+DrawPoint noFigure::InterpolateWalkDrawPos(DrawPoint drawPt) const
+{
+    // Add an offset relative to the starting point calculated by how far we already walked
+    // Don't if we are waiting on our starting node
     if(!waiting_for_free_node || IsStoppedBetweenNodes())
         drawPt += CalcFigurRelative();
+    return drawPt;
+}
 
-    LOADER.carrier_cache[ware][GetCurMoveDir().toUInt()][ani_step][fat].draw(drawPt, COLOR_WHITE, gwg->GetPlayer(player).color);
+void noFigure::DrawWalkingBobCarrier(DrawPoint drawPt, unsigned ware, bool fat)
+{
+    const unsigned ani_step = CalcWalkAnimationFrame();
+
+    LOADER.carrier_cache[ware][GetCurMoveDir().toUInt()][ani_step][fat].drawForPlayer(InterpolateWalkDrawPos(drawPt),
+                                                                                      gwg->GetPlayer(player).color);
 }
 
 void noFigure::DrawWalkingBobJobs(DrawPoint drawPt, unsigned job)
 {
-    if((job == JOB_SCOUT) || ((job >= JOB_PRIVATE) && (job <= JOB_GENERAL)))
-    {
-        DrawWalking(drawPt, LOADER.GetBobN("jobs"), JOB_CONSTS[job].jobs_bob_id + NATION_RTTR_TO_S2[gwg->GetPlayer(player).nation] * 6,
-                    false);
-        return;
-    }
+    const unsigned ani_step = CalcWalkAnimationFrame();
 
-    // Wenn wir warten auf ein freies Plätzchen, müssen wir den stehend zeichnen!
-    unsigned ani_step = waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
-
-    // Wenn man wartet, stehend zeichnen, es sei denn man wartet mittem auf dem Weg!
-    if(!waiting_for_free_node || IsStoppedBetweenNodes())
-        drawPt += CalcFigurRelative();
-
-    LOADER.bob_jobs_cache[gwg->GetPlayer(player).nation][job][GetCurMoveDir().toUInt()][ani_step].draw(drawPt, 0xFFFFFFFF,
-                                                                                                       gwg->GetPlayer(player).color);
+    const GamePlayer& owner = gwg->GetPlayer(player);
+    LOADER.bob_jobs_cache[owner.nation][job][GetCurMoveDir().toUInt()][ani_step].drawForPlayer(InterpolateWalkDrawPos(drawPt), owner.color);
 }
 
-void noFigure::DrawWalking(DrawPoint drawPt, glArchivItem_Bob* file, unsigned id, bool fat, bool waitingsoldier)
+void noFigure::DrawWalking(DrawPoint drawPt, glArchivItem_Bob* file, unsigned id, bool fat)
 {
-    // Wenn wir warten auf ein freies Plätzchen, müssen wir den stehend zeichnen!
-    unsigned ani_step =
-      waiting_for_free_node || waitingsoldier ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
+    const unsigned ani_step = CalcWalkAnimationFrame();
+    drawPt = InterpolateWalkDrawPos(drawPt);
 
-    // Wenn man wartet, stehend zeichnen, es sei denn man wartet mittem auf dem Weg!
-    if(!waitingsoldier && (!waiting_for_free_node || IsStoppedBetweenNodes()))
-        drawPt += CalcFigurRelative();
     if(file)
         file->Draw(id, GetCurMoveDir().toUInt(), fat, ani_step, drawPt, gwg->GetPlayer(player).color);
     DrawShadow(drawPt, ani_step, GetCurMoveDir());
@@ -801,12 +794,8 @@ void noFigure::DrawWalking(DrawPoint drawPt, glArchivItem_Bob* file, unsigned id
 /// Zeichnet standardmäßig die Figur, wenn sie läuft aus einem bestimmten normalen LST Archiv
 void noFigure::DrawWalking(DrawPoint drawPt, const char* const file, unsigned id)
 {
-    // Wenn wir warten, ani-step 2 benutzen
-    unsigned ani_step = waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
-
-    // Wenn man wartet, stehend zeichnen, es sei denn man wartet mittem auf dem Weg!
-    if(!waiting_for_free_node || IsStoppedBetweenNodes())
-        drawPt += CalcFigurRelative();
+    const unsigned ani_step = CalcWalkAnimationFrame();
+    drawPt = InterpolateWalkDrawPos(drawPt);
 
     LOADER.GetPlayerImage(file, id + (GetCurMoveDir() + 3u).toUInt() * 8 + ani_step)
       ->DrawFull(drawPt, COLOR_WHITE, gwg->GetPlayer(player).color);
@@ -820,25 +809,17 @@ void noFigure::DrawWalking(DrawPoint drawPt)
     {
         case JOB_PACKDONKEY:
         {
-            // Wenn wir warten, ani-step 2 benutzen
-            unsigned ani_step = waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
-
-            // Wenn man wartet, stehend zeichnen, es sei denn man wartet mittem auf dem Weg!
-            if(!waiting_for_free_node || IsStoppedBetweenNodes())
-                drawPt += CalcFigurRelative();
+            const unsigned ani_step = CalcWalkAnimationFrame();
+            drawPt = InterpolateWalkDrawPos(drawPt);
 
             // Esel
             LOADER.GetMapImageN(2000 + (GetCurMoveDir() + 3u).toUInt() * 8 + ani_step)->DrawFull(drawPt);
             // Schatten des Esels
             LOADER.GetMapImageN(2048 + GetCurMoveDir().toUInt() % 3)->DrawFull(drawPt, COLOR_SHADOW);
         }
-            return;
-        case JOB_CHARBURNER: { DrawWalking(drawPt, "charburner_bobs", 53);
-        }
-            return;
-        default: { DrawWalkingBobJobs(drawPt, job_);
-        }
-            return;
+        break;
+        case JOB_CHARBURNER: DrawWalking(drawPt, "charburner_bobs", 53); break;
+        default: DrawWalkingBobJobs(drawPt, job_); break;
     }
 }
 

libs/s25main/figures/noFigure.h
@@ -156,12 +156,16 @@ public:
     Job GetJobType() const { return job_; }
     /// Returns true if this is a soldier (they get some special handling at some points)
     bool IsSoldier() const;
+
+    unsigned CalcWalkAnimationFrame() const;
+    DrawPoint InterpolateWalkDrawPos(DrawPoint drawPt) const;
+
     /// Zeichnet eine Figur aus "carrier.bob" beim Laufen.
     void DrawWalkingBobCarrier(DrawPoint drawPt, unsigned ware, bool fat);
     /// Zeichnet eine Figur aus "jobs.bob", wenn sie läuft.
     void DrawWalkingBobJobs(DrawPoint drawPt, unsigned job);
     /// Zeichnet standardmäßig die Figur, wenn sie läuft
-    void DrawWalking(DrawPoint drawPt, glArchivItem_Bob* file, unsigned id, bool fat, bool waitingsoldier = false);
+    void DrawWalking(DrawPoint drawPt, glArchivItem_Bob* file, unsigned id, bool fat);
     /// Zeichnet standardmäßig die Figur, wenn sie läuft aus einem bestimmten normalen LST Archiv
     void DrawWalking(DrawPoint drawPt, const char* file, unsigned id);
     /// Zeichnet standardmäßig die Figur, wenn sie läuft, nimmt automatisch richtige Job-ID/Datei

libs/s25main/figures/nofActiveSoldier.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2020 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
 //
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
 
-#include "rttrDefines.h" // IWYU pragma: keep
 #include "nofActiveSoldier.h"
 #include "EventManager.h"
 #include "GamePlayer.h"
@@ -152,7 +151,7 @@ void nofActiveSoldier::Draw(DrawPoint drawPt)
         case STATE_DEFENDING_WAITING:
         {
             // Draw waiting states
-            DrawSoldierWalking(drawPt, true); // cannot draw from Soldiers & Scouts from Loader::bob_jobs_cache v9102
+            DrawSoldierWaiting(drawPt);
         }
         break;
         case STATE_FIGUREWORK:
@@ -169,7 +168,7 @@ void nofActiveSoldier::Draw(DrawPoint drawPt)
         case STATE_SEAATTACKING_RETURNTOSHIP:
         {
             // Draw walking states
-            DrawSoldierWalking(drawPt);
+            DrawWalkingBobJobs(drawPt, job_);
         }
         break;
     }
@@ -495,7 +494,3 @@ void nofActiveSoldier::FightingStarted()
     state = STATE_FIGHTING;
     enemy = nullptr;
 }
-
-///////////////////////////////////////////////////////////////////////////////
-// EOF
-///////////////////////////////////////////////////////////////////////////////

libs/s25main/figures/nofCarrier.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2005 - 2017 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2020 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
 //
@@ -15,9 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
 
-#include "rttrDefines.h" // IWYU pragma: keep
 #include "nofCarrier.h"
-
 #include "EventManager.h"
 #include "GamePlayer.h"
 #include "Loader.h"
@@ -272,9 +270,7 @@ void nofCarrier::Draw(DrawPoint drawPt)
                 LOADER.GetMapImageN(2350 + carried_ware->type)->DrawFull(drawPt + WARE_POS_DONKEY[GetCurMoveDir().toUInt()][0]);
             } else
             {
-                // Wenn wir warten auf ein freies Plätzchen, müssen wir den stehend zeichnen!
-                // Wenn event = 0, dann sind wir mittem auf dem Weg angehalten!
-                unsigned ani_step = waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
+                const unsigned ani_step = CalcWalkAnimationFrame();
 
                 drawPt += CalcFigurRelative();
 
@@ -308,9 +304,7 @@ void nofCarrier::Draw(DrawPoint drawPt)
                 LOADER.GetMapImageN(2350 + carried_ware->type)->DrawFull(drawPt + WARE_POS_BOAT[GetCurMoveDir().toUInt()]);
             } else
             {
-                // Wenn wir warten auf ein freies Plätzchen, müssen wir den (fest)stehend zeichnen!
-                // Wenn event = 0, dann sind wir mittem auf dem Weg angehalten!
-                unsigned ani_step = waiting_for_free_node ? 2 : GAMECLIENT.Interpolate(ASCENT_ANIMATION_STEPS[GetAscent()], current_ev) % 8;
+                const unsigned ani_step = CalcWalkAnimationFrame();
 
                 drawPt += CalcFigurRelative();
 

commit 859306c48152b1ed5007d316dcdfbf94e11e1173

Fix wrong image used for workers walking with ware
Regression from f3257cfca78d8c9ce589726c7f87ade6a3592276

libs/s25main/figures/nofBuildingWorker.cpp
@@ -19,11 +19,13 @@
 #include "EventManager.h"
 #include "GamePlayer.h"
 #include "Loader.h"
+#include "RTTR_Assert.h"
 #include "SerializedGameData.h"
 #include "SoundManager.h"
 #include "Ware.h"
 #include "buildings/nobBaseWarehouse.h"
 #include "buildings/nobUsual.h"
+#include "helpers/MaxEnumValue.h"
 #include "world/GameWorldGame.h"
 #include "nodeObjs/noFlag.h"
 #include "gameData/JobConsts.h"
@@ -319,7 +321,10 @@ void nofBuildingWorker::DrawWalkingWithWare(DrawPoint drawPt)
     unsigned short id = GetCarryID();
     // >=100 -> carrier.bob else jobs.bob!
     if(id >= 100)
-        DrawWalkingBobCarrier(drawPt, id - 100, JOB_SPRITE_CONSTS[job_].isFat());
-    else
-        DrawWalkingBobJobs(drawPt, job_);
+    {
+        id -= 100;
+        RTTR_Assert(id <= helpers::MaxEnumValue_v<GoodType>);
+        DrawWalkingBobCarrier(drawPt, GoodType(id), JOB_SPRITE_CONSTS[job_].isFat());
+    } else
+        DrawWalking(drawPt, LOADER.GetBob("jobs"), id, JOB_SPRITE_CONSTS[job_].isFat());
 }

libs/s25main/figures/nofWellguy.h
@@ -32,7 +32,7 @@ protected:
     void DrawWorking(DrawPoint drawPt) override;
     /// Gibt die ID in JOBS.BOB zurück, wenn der Beruf Waren rausträgt (bzw rein)
     // TODO:der Brunnentyphat keine ID in JOBS.BOB
-    unsigned short GetCarryID() const override { return 111; }
+    unsigned short GetCarryID() const override { return 100 + GD_WATER; }
     /// Der Arbeiter erzeugt eine Ware
     GoodType ProduceWare() override;
 