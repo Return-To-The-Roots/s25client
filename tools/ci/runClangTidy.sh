489e8ba8cada8a310e33c20932808b0c7533ce2b
Alexander Grund <Flamefire@users.noreply.github.com>
2020-09-15 21:06:01 +0200

    Do only a single clang-tidy run

--- tools/ci/runClangTidy.sh
+++ tools/ci/runClangTidy.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-WHAT="${1}"
-
 mkdir -p build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug \
     -DRTTR_EDITOR_ADMINMODE=ON \
@@ -11,25 +9,12 @@ cmake .. -DCMAKE_BUILD_TYPE=Debug \
     ${ADDITIONAL_CMAKE_FLAGS}
 
 SRC_DIR="$(cd .. && pwd)"
-MAIN_FILTER="extras|libs|tests"
-EXTERNAL_FILTER="external/(libendian|liblobby|libsiedler2|mygettext|s25edit|s25update)|\
-external/libutil/(tests|lib)"
-HEADER_FILTER="${MAIN_FILTER}|${EXTERNAL_FILTER}"
-
-if [[ "${WHAT}" == "main" ]]; then
-    SRC_FILTER="${MAIN_FILTER}"
-elif [[ "${WHAT}" == "external" ]]; then
-    SRC_FILTER="${EXTERNAL_FILTER}"
-else
-    echo "Invalid param: ${WHAT}"
-    exit 1
-fi
+FILTER="extras|libs|tests|external/(libendian|liblobby|libsiedler2|libutil|mygettext|s25edit|s25update)"
 
 script -q -c "run-clang-tidy-10 -p . \
-    -j2 \
     -quiet \
-    -header-filter \"${SRC_DIR}/(${HEADER_FILTER})\" \
-    \"${SRC_DIR}/(${SRC_FILTER})\"" /dev/null \
+    -header-filter \"${SRC_DIR}/(${FILTER})\" \
+    \"${SRC_DIR}/(${FILTER})\"" /dev/null \
      2>&1 | tee tidy-output.log | grep -v ' warnings generated.'
 
 ERRORS="$( (grep -E 'warning: |error: ' tidy-output.log || [ $? -eq 1 ]) | sort -u)"
