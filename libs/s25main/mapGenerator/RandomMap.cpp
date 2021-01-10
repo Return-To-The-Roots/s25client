30d62838c92bf672c897115250e303cf454f4dff
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-01-10 18:58:12 +0000

    Use point parameter to avoid CI error

--- libs/s25main/mapGenerator/RandomMap.cpp
+++ libs/s25main/mapGenerator/RandomMap.cpp
@@ -272,7 +272,7 @@ namespace rttr { namespace mapGenerator {
 
     void RandomMap::CreateLandMap()
     {
-        Restructure(map_, [this](const MapPoint& _) { return _.x >= 0 && rnd_.ByChance(5); });
+        Restructure(map_, [this](const MapPoint& pt) { return rnd_.ByChance((pt.x + pt.y) % 6); });
         SmoothHeightMap(map_.z, map_.height);
 
         const double sea = rnd_.RandomDouble(0.1, 0.2);
