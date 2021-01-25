4bac5c31cc0ee032117df61c222116bba633345e
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-01-25 13:01:36 +0000

    Removed member initialization from MapSettings constructor

--- libs/s25main/mapGenerator/MapSettings.h
+++ libs/s25main/mapGenerator/MapSettings.h
@@ -41,24 +41,19 @@ namespace rttr { namespace mapGenerator {
 
     struct MapSettings
     {
-        MapSettings()
-            : numPlayers(2), size(MapExtent::all(128)), ratioGold(9), ratioIron(36), ratioCoal(40), ratioGranite(15),
-              rivers(15), mountainDistance(MountainDistance::Normal), type(0), style(MapStyle::Mixed)
-        {}
-
         void MakeValid();
 
         std::string name, author;
-        unsigned numPlayers;
-        MapExtent size;
-        unsigned short ratioGold;
-        unsigned short ratioIron;
-        unsigned short ratioCoal;
-        unsigned short ratioGranite;
-        unsigned short rivers;
-        MountainDistance mountainDistance;
-        DescIdx<LandscapeDesc> type;
-        MapStyle style;
+        unsigned numPlayers = 2;
+        MapExtent size = MapExtent::all(128);
+        unsigned short ratioGold = 9;
+        unsigned short ratioIron = 36;
+        unsigned short ratioCoal = 40;
+        unsigned short ratioGranite = 15;
+        unsigned short rivers = 15;
+        MountainDistance mountainDistance = MountainDistance::Normal;
+        DescIdx<LandscapeDesc> type = DescIdx<LandscapeDesc>(0);
+        MapStyle style = MapStyle::Mixed;
     };
 
 }} // namespace rttr::mapGenerator
