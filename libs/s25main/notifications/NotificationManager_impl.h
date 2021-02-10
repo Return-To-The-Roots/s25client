80a2be60c1a66dd28714ef6ffbd97e320f83a251
Flamefire <Flamefire@users.noreply.github.com>
2021-02-10 10:43:17 +0100

    Handle iterator (but not reference) invalidation on push to deque

--- libs/s25main/notifications/NotificationManager_impl.h
+++ libs/s25main/notifications/NotificationManager_impl.h
@@ -22,6 +22,7 @@
 #    include "notifications/NotificationManager.h"
 #endif
 #include "RTTR_Assert.h"
+#include "helpers/containerUtils.h"
 #include <algorithm>
 #include <stdexcept>
 #include <utility>
@@ -48,14 +49,16 @@ struct NotificationManager::NoteCallback final : NoteCallbackBase
 
 inline NotificationManager::~NotificationManager()
 {
-    RTTR_Assert(!isPublishing);
     // Unsubscribe all callbacks so we don't get accesses to this class after destruction
     for(const auto& subscribers : noteId2Subscriber)
-        for(void* callback : subscribers.second)
+    {
+        RTTR_Assert(!subscribers.second.isPublishing);
+        for(void* callback : subscribers.second.callbacks)
         {
             if(callback)
                 static_cast<NoteCallbackBase*>(callback)->SetUnsubscribed();
         }
+    }
 }
 
 inline void NotificationManager::unsubscribe(Subscription& subscription) noexcept
@@ -68,7 +71,7 @@ template<class T_Note>
 Subscription NotificationManager::subscribe(std::function<void(const T_Note&)> callback) noexcept
 {
     auto* subscriber = new NoteCallback<T_Note>(std::move(callback));
-    noteId2Subscriber[T_Note::getNoteId()].push_back(subscriber);
+    noteId2Subscriber[T_Note::getNoteId()].callbacks.push_back(subscriber);
 
     return Subscription(subscriber, [this](void* subscription) {
         RTTR_Assert(subscription); // As we use this in a shared_ptr, this can never be nullptr
@@ -83,11 +86,12 @@ template<class T_Note>
 void NotificationManager::unsubscribe(NoteCallback<T_Note>* callback) noexcept
 {
     RTTR_Assert(callback->IsSubscribed());
-    CallbackList& callbacks = noteId2Subscriber[T_Note::getNoteId()];
+    Subscribers& subs = noteId2Subscriber[T_Note::getNoteId()];
+    CallbackList& callbacks = subs.callbacks;
     auto itEl = std::find(callbacks.begin(), callbacks.end(), callback);
     RTTR_Assert(itEl != callbacks.end());
     // We can't modify the list while iterating over it
-    if(isPublishing)
+    if(subs.isPublishing)
         *itEl = nullptr;
     else
         callbacks.erase(itEl);
@@ -98,29 +102,32 @@ void NotificationManager::unsubscribe(NoteCallback<T_Note>* callback) noexcept
 template<class T_Note>
 void NotificationManager::publish(const T_Note& notification)
 {
-    ++isPublishing;
+    Subscribers& subs = noteId2Subscriber[T_Note::getNoteId()];
+    ++subs.isPublishing;
     try
     {
         // Note: We have to support subscribe and unsubscribe during the execute call.
         // - noteId2Subscriber must have elements with stable adresses even on insertion (subscribe)
-        // - CallbackList must not invalidate any iterator on insert
+        // - CallbackList must not invalidate any references on insert
         // - Since erase usually invalidates iterators unsubscribe only clears the pointer which we have to handle here
-        CallbackList& callbacks = noteId2Subscriber[T_Note::getNoteId()];
-        for(auto it = callbacks.begin(); it != callbacks.end();)
+        CallbackList& callbacks = subs.callbacks;
+        bool hasEmpty = false;
+        for(unsigned i = 0; i < callbacks.size(); ++i)
         {
-            if(*it)
-            {
-                static_cast<NoteCallback<T_Note>*>(*it)->execute(notification);
-                ++it;
-            } else
-                it = callbacks.erase(it);
+            auto* cb = static_cast<NoteCallback<T_Note>*>(callbacks[i]);
+            if(cb)
+                cb->execute(notification);
+            else
+                hasEmpty = true;
         }
-        RTTR_Assert(isPublishing);
-        --isPublishing;
+        RTTR_Assert(subs.isPublishing);
+        --subs.isPublishing;
+        if(!subs.isPublishing && hasEmpty)
+            helpers::erase(callbacks, nullptr);
     } catch(...)
     {
-        RTTR_Assert(isPublishing);
-        --isPublishing;
+        RTTR_Assert(subs.isPublishing);
+        --subs.isPublishing;
         throw;
     }
 }
