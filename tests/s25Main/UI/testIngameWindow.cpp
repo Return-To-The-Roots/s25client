47cdfdc4b76743496289b4db507fc8f821825b5a
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-02-15 16:57:21 +0000

    Improved unit tests for iwMapGenerator class

--- tests/s25Main/UI/testIngameWindow.cpp
+++ tests/s25Main/UI/testIngameWindow.cpp
@@ -20,6 +20,8 @@
 #include "controls/ctrlProgress.h"
 #include "ingameWindows/iwHelp.h"
 #include "ingameWindows/iwMapGenerator.h"
+#include "mapGenerator/MapSettings.h"
+#include "rttr/test/random.hpp"
 #include "uiHelper/uiHelpers.hpp"
 #include <boost/test/unit_test.hpp>
 
@@ -70,36 +72,45 @@ BOOST_AUTO_TEST_CASE(IwMapGenerator)
         CTRL_STONE_PILES,
         CTRL_ISLANDS
     };
+    const auto expectedNumPlayers = rttr::test::randomValue(2u, 7u);
+    const auto expectedMapType = rttr::test::randomValue(0, 2);
+    const auto expectedGoldRatio = rttr::test::randomValue(0u, 100u);
+    const auto expectedIronRatio = rttr::test::randomValue(0u, 100u);
+    const auto expectedCoalRatio = rttr::test::randomValue(0u, 100u);
+    const auto expectedGraniteRatio = rttr::test::randomValue(0u, 100u);
+    const auto expectedRivers = rttr::test::randomValue(0u, 100u);
+    const auto expectedTrees = rttr::test::randomValue(0u, 100u);
+    const auto expectedStonePiles = rttr::test::randomValue(0u, 100u);
 
     uiHelper::initGUITests();
     rttr::mapGenerator::MapSettings settings;
     iwMapGenerator wnd(settings);
-    wnd.GetCtrl<ctrlComboBox>(CTRL_PLAYER_NUMBER)->SetSelection(2); // 4 players
-    wnd.GetCtrl<ctrlComboBox>(CTRL_MAP_TYPE)->SetSelection(2);      // LandscapeDesc(2)
+    wnd.GetCtrl<ctrlComboBox>(CTRL_PLAYER_NUMBER)->SetSelection(expectedNumPlayers-2);
+    wnd.GetCtrl<ctrlComboBox>(CTRL_MAP_TYPE)->SetSelection(expectedMapType);
     wnd.GetCtrl<ctrlComboBox>(CTRL_MAP_STYLE)->SetSelection(1);     // MapStyle::Land
     wnd.GetCtrl<ctrlComboBox>(CTRL_MAP_SIZE)->SetSelection(4);      // 1024x1024
     wnd.GetCtrl<ctrlComboBox>(CTRL_MOUNTAIN_DIST)->SetSelection(3); // MountainDistance::VeryFar
     wnd.GetCtrl<ctrlComboBox>(CTRL_ISLANDS)->SetSelection(2);       // IslandAmount::Many
-    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_GOLD)->SetPosition(25);
-    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_IRON)->SetPosition(25);
-    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_COAL)->SetPosition(25);
-    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_GRANITE)->SetPosition(25);
-    wnd.GetCtrl<ctrlProgress>(CTRL_RIVERS)->SetPosition(33);
-    wnd.GetCtrl<ctrlProgress>(CTRL_TREES)->SetPosition(33);
-    wnd.GetCtrl<ctrlProgress>(CTRL_STONE_PILES)->SetPosition(33);
+    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_GOLD)->SetPosition(expectedGoldRatio);
+    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_IRON)->SetPosition(expectedIronRatio);
+    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_COAL)->SetPosition(expectedCoalRatio);
+    wnd.GetCtrl<ctrlProgress>(CTRL_RATIO_GRANITE)->SetPosition(expectedGraniteRatio);
+    wnd.GetCtrl<ctrlProgress>(CTRL_RIVERS)->SetPosition(expectedRivers);
+    wnd.GetCtrl<ctrlProgress>(CTRL_TREES)->SetPosition(expectedTrees);
+    wnd.GetCtrl<ctrlProgress>(CTRL_STONE_PILES)->SetPosition(expectedStonePiles);
     wnd.Window::Msg_ButtonClick(CTRL_BTN_APPLY);
 
-    BOOST_TEST(settings.numPlayers == 4);
-    BOOST_TEST(settings.type == DescIdx<LandscapeDesc>(2));
+    BOOST_TEST(settings.numPlayers == expectedNumPlayers);
+    BOOST_TEST(settings.type == DescIdx<LandscapeDesc>(expectedMapType));
     BOOST_TEST(settings.size == MapExtent(1024, 1024));
     BOOST_TEST(settings.style == rttr::mapGenerator::MapStyle::Land);
     BOOST_TEST(settings.mountainDistance == rttr::mapGenerator::MountainDistance::VeryFar);
     BOOST_TEST(settings.islands == rttr::mapGenerator::IslandAmount::Many);
-    BOOST_TEST(settings.ratioGold == 25);
-    BOOST_TEST(settings.ratioIron == 25);
-    BOOST_TEST(settings.ratioCoal == 25);
-    BOOST_TEST(settings.ratioGranite == 25);
-    BOOST_TEST(settings.rivers == 33);
-    BOOST_TEST(settings.trees == 33);
-    BOOST_TEST(settings.stonePiles == 33);
+    BOOST_TEST(settings.ratioGold == expectedGoldRatio);
+    BOOST_TEST(settings.ratioIron == expectedIronRatio);
+    BOOST_TEST(settings.ratioCoal == expectedCoalRatio);
+    BOOST_TEST(settings.ratioGranite == expectedGraniteRatio);
+    BOOST_TEST(settings.rivers == expectedRivers);
+    BOOST_TEST(settings.trees == expectedTrees);
+    BOOST_TEST(settings.stonePiles == expectedStonePiles);
 }
