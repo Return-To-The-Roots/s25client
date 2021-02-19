1ba05201835c4bbfa32e693b3abf113ef8709be7
Alexander Grund <Flamefire@users.noreply.github.com>
2021-02-19 20:05:00 +0100

    Rename max to maxExcl to show that this value is not included

--- libs/s25main/random/randomIO.cpp
+++ libs/s25main/random/randomIO.cpp
@@ -26,9 +26,9 @@ std::ostream& operator<<(std::ostream& os, const RandomEntry& entry)
     std::string strippedSrcFile = boost::filesystem::path(entry.srcName).generic_string();
     if(strippedSrcFile.find(rttrSrcBaseName) == 0)
         strippedSrcFile = strippedSrcFile.substr(rttrSrcBaseName.size());
-    return os << entry.counter << ":R(" << entry.max << ")=" << entry.GetValue() << ",z=" << std::hex << std::setw(8)
-              << entry.rngState << std::dec << std::setw(0) << "\t| " << strippedSrcFile << "#" << entry.srcLine
-              << "\t| id=" << entry.objId;
+    return os << entry.counter << ":R(" << entry.maxExcl << ")=" << entry.GetValue() << ",z=" << std::hex
+              << std::setw(8) << entry.rngState << std::dec << std::setw(0) << "\t| " << strippedSrcFile << "#"
+              << entry.srcLine << "\t| id=" << entry.objId;
 }
 
 void saveRandomLog(const boost::filesystem::path& filepath, const std::vector<RandomEntry>& log)
