aeb0cc21b8e1e818d2b50e82adcb53327957cf03
Johannes Doerfert <johannes@jdoerfert.de>
2021-02-21 16:01:28 -0600

    Allow to query the amount on geologist signs

--- libs/s25main/nodeObjs/noSign.h
+++ libs/s25main/nodeObjs/noSign.h
@@ -40,6 +40,10 @@ public:
     /// An x,y zeichnen.
     void Draw(DrawPoint drawPt) override;
 
+    /// Return the amount of resources found, as drawn on the sign.
+    uint8_t GetSignAmount() const { return resource.getAmount(); }
+
+    /// Return the type of resources found, as drawn on the sign.
     ResourceType GetSignType() const { return resource.getType(); }
 
 private:
