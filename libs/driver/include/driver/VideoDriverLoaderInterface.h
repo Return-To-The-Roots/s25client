dde3c338bf11c611083dd6e5b957b3ccb566d7b8
Flamefire <Flamefire@users.noreply.github.com>
2021-04-20 18:37:20 +0200

    Switch to SPDX license identifier

--- libs/driver/include/driver/VideoDriverLoaderInterface.h
+++ libs/driver/include/driver/VideoDriverLoaderInterface.h
@@ -1,19 +1,6 @@
 // Copyright (c) 2005-2009 Settlers Freaks (sf-team at siedler25.org)
 //
-// This file is part of Siedler II.5 RTTR.
-//
-// Siedler II.5 RTTR is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// Siedler II.5 RTTR is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Siedler II.5 RTTR. If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
--- libs/s25main/drivers/ScreenResizeEvent.h
+++ libs/s25main/drivers/ScreenResizeEvent.h
@@ -1,17 +1,6 @@
 // Copyright (c) 2005 - 2017 Settlers Freaks (sfteam at siedler25.org)
 //
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 2, or (at your option) any
-// later version.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
