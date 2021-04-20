85af9bf767c80cdd8d0558fe85348a4f078dc747
Flamefire <Flamefire@users.noreply.github.com>
2021-04-20 18:37:20 +0200

    Switch to SPDX license identifier

--- tests/s25Main/integration/testGamePlayer.cpp
+++ tests/s25Main/integration/testGamePlayer.cpp
@@ -1,19 +1,6 @@
 // Copyright (c) 2016 - 2018 Settlers Freaks (sf-team at siedler25.org)
 //
-// This file is part of Return To The Roots.
-//
-// Return To The Roots is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// Return To The Roots is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Return To The Roots. If not, see <http://www.gnu.org/licenses/>.
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "GamePlayer.h"
 #include "buildings/nobBaseWarehouse.h"
