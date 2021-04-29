4ce6199a77ebb44b5b14cdc23c0e0a19e41c77ca
Alexander Grund <Flamefire@users.noreply.github.com>
2021-04-29 14:44:14 +0200

    Update Copyright start date to 2005
    First release date of RttR as suggested by @Flow86

--- tools/RunTests.bat
+++ tools/RunTests.bat
@@ -1,4 +1,4 @@
-REM Copyright (C) 2018 - 2021 Settlers Freaks <sf-team at siedler25.org>
+REM Copyright (C) 2005 - 2021 Settlers Freaks <sf-team at siedler25.org>
 REM
 REM SPDX-License-Identifier: GPL-2.0-or-later
 
--- tools/release/debian/fix-changelog.php
+++ tools/release/debian/fix-changelog.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 
-// Copyright (C) 2018 - 2021 Settlers Freaks <sf-team at siedler25.org>
+// Copyright (C) 2005 - 2021 Settlers Freaks <sf-team at siedler25.org>
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
