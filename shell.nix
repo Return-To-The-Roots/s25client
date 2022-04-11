b12db9cefedc0de3e22004d8758a9142df5ab3e9
Shawn8901 <shawn8901@googlemail.com>
2022-04-11 19:18:36 +0200

    Nix shell: Set SOURCE_DATE_EPOCH to newest file
    RTTR does use epoch on a sanity unit test and when calculating RTTR_BUILD_DATE and RTTR_VERSION on non-release builds.
    Nix does set SOURCE_DATE_EPOCH to the static value 315532800 by default (1980-01-01).
    That epoch does not pass the sanity unit test.
    
    By using the updateSourceDateEpoch the SOURCE_DATE_EPOCH is updated to the newest file in the folder.
    See https://github.com/NixOS/nixpkgs/blob/master/pkgs/build-support/setup-hooks/set-source-date-epoch-to-latest.sh for implementation details

--- shell.nix
+++ shell.nix
@@ -18,7 +18,15 @@
 with pkgs;
 mkShell {
   name = "s25client";
+
+  # Set SOURCE_DATE_EPOCH to the newest file in the directory.
+  # RTTR_BUILD_DATE is calculated by that epoch value.
+  shellHook = ''
+    updateSourceDateEpoch .
+  '';
+
   buildInputs = [
+    stdenv
     boost
     bzip2
     cmake
