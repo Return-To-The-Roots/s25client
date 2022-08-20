c6ba939b6a28e84d50bdb73b89c41d447abfbbc1
KaiN <kain@piwnica.ws>
2022-08-20 17:25:43 +0200

    Fix loading plugin graphics, e.g. nation-specific catapult graphics

--- libs/s25main/addons/AddonCatapultGraphics.h
+++ libs/s25main/addons/AddonCatapultGraphics.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2005 - 2021 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (C) 2005 - 2022 Settlers Freaks (sf-team at siedler25.org)
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -8,7 +8,7 @@
 #include "mygettext/mygettext.h"
 
 /**
- *  Addon for new race specific catapult graphics.
+ *  Addon for new nation-specific catapult graphics.
  *
  *  Those graphics were made by Parasit.
  */
@@ -16,7 +16,7 @@ class AddonCatapultGraphics : public AddonBool
 {
 public:
     AddonCatapultGraphics()
-        : AddonBool(AddonId::CATAPULT_GRAPHICS, AddonGroup::GamePlay, _("Race specific catapult graphics"),
+        : AddonBool(AddonId::CATAPULT_GRAPHICS, AddonGroup::GamePlay, _("Nation-specific catapult graphics"),
                     _("Adds new race-specific graphics for catapults to the game."))
     {}
 };
