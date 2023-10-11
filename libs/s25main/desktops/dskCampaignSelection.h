2dc5e3b1f88c001d5a5fe441bcb808055e07d22d
Sunrise <schafli@wolke7.net>
2023-10-11 11:38:59 +0200

    Add campaign menu and mission selection screen

---
+++ libs/s25main/desktops/dskCampaignSelection.h
@@ -0,0 +1,33 @@
+// Copyright (C) 2005 - 2023 Settlers Freaks (sf-team at siedler25.org)
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "desktops/Desktop.h"
+#include "network/CreateServerInfo.h"
+#include <boost/filesystem/path.hpp>
+#include <set>
+
+class dskCampaignSelection : public Desktop
+{
+public:
+    dskCampaignSelection(CreateServerInfo csi);
+
+protected:
+    void Draw_() override;
+
+private:
+    void Msg_TableChooseItem(unsigned, unsigned) override;
+    void Msg_TableSelectItem(unsigned ctrl_id, const boost::optional<unsigned>& selection) override;
+    void Msg_ButtonClick(unsigned ctrl_id) override;
+    void Msg_Timer(unsigned ctrl_id) override;
+    void FillCampaignsTable();
+    void showCampaignInfo(bool show);
+    boost::filesystem::path getSelectedCampaignPath();
+    bool showCampaignInfo_;
+    CreateServerInfo csi_;
+    glArchivItem_Bitmap* campaignImage_;
+    /// Campaigns that we already know are broken
+    std::set<boost::filesystem::path> brokenCampaignPaths_;
+};
