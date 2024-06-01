7bf8281a41e094ad1aa43d574da489a13afe1958
Sunrise <schafli@wolke7.net>
2024-06-01 22:49:58 +0200

    Consistently use GetWareTex and GetWareTexStack instead of GetMapTexture
    Makes adding new textures for additional wares much easier
    because we have only one place to add a lookup for the new texture.

--- libs/s25main/buildings/nobHarborBuilding.cpp
+++ libs/s25main/buildings/nobHarborBuilding.cpp
@@ -244,13 +244,11 @@ void nobHarborBuilding::Draw(DrawPoint drawPt)
         // Bretter
         DrawPoint boardsPos = drawPt + BOARDS_POS[nation];
         for(unsigned char i = 0; i < expedition.boards; ++i)
-            LOADER.GetMapTexture(WARE_STACK_TEX_MAP_OFFSET + rttr::enum_cast(GoodType::Boards))
-              ->DrawFull(boardsPos - DrawPoint(0, i * 4));
+            LOADER.GetWareStackTex(GoodType::Boards)->DrawFull(boardsPos - DrawPoint(0, i * 4));
         DrawPoint stonesPos = drawPt + STONES_POS[nation];
         // Steine
         for(unsigned char i = 0; i < expedition.stones; ++i)
-            LOADER.GetMapTexture(WARE_STACK_TEX_MAP_OFFSET + rttr::enum_cast(GoodType::Stones))
-              ->DrawFull(stonesPos - DrawPoint(0, i * 4));
+            LOADER.GetWareStackTex(GoodType::Stones)->DrawFull(stonesPos - DrawPoint(0, i * 4));
 
         // Und den Bauarbeiter, falls er schon da ist
         if(expedition.builder)
