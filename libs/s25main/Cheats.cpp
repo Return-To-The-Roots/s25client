f6baefcbbd643d72dc6ffd3648c877e65840d681
Jakub Audykowicz <jakub.audykowicz@gmail.com>
2024-08-05 17:20:34 +0200

    Apply review remarks:
    - avoid early returns
    - rename cheatStr to enableCheatsStr
    - rename canCheatModeBeOn() to areCheatsAllowed()
    - trackCharKeyEvent return void
    - hold cheatCmdTracker_ by value instead of unique_ptr

--- libs/s25main/Cheats.cpp
+++ libs/s25main/Cheats.cpp
@@ -3,58 +3,44 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "Cheats.h"
-#include "CheatCommandTracker.h"
 #include "network/GameClient.h"
 #include "world/GameWorldBase.h"
 
-Cheats::Cheats(GameWorldBase& world) : cheatCmdTracker_(std::make_unique<CheatCommandTracker>(*this)), world_(world) {}
+Cheats::Cheats(GameWorldBase& world) : cheatCmdTracker_(*this), world_(world) {}
 
 Cheats::~Cheats() = default;
 
 void Cheats::trackKeyEvent(const KeyEvent& ke)
 {
-    if(!canCheatModeBeOn())
-        return;
-
-    cheatCmdTracker_->trackKeyEvent(ke);
+    if(areCheatsAllowed())
+        cheatCmdTracker_.trackKeyEvent(ke);
 }
 
 void Cheats::trackChatCommand(const std::string& cmd)
 {
-    if(!canCheatModeBeOn())
-        return;
-
-    cheatCmdTracker_->trackChatCommand(cmd);
+    if(areCheatsAllowed())
+        cheatCmdTracker_.trackChatCommand(cmd);
 }
 
 void Cheats::toggleCheatMode()
 {
-    if(!canCheatModeBeOn())
-        return;
-
-    isCheatModeOn_ = !isCheatModeOn_;
+    if(areCheatsAllowed())
+        isCheatModeOn_ = !isCheatModeOn_;
 }
 
 void Cheats::toggleHumanAIPlayer()
 {
-    if(!isCheatModeOn())
-        return;
-
-    if(GAMECLIENT.IsReplayModeOn())
-        return;
-
-    GAMECLIENT.ToggleHumanAIPlayer(AI::Info{AI::Type::Default, AI::Level::Easy});
+    if(isCheatModeOn() && !GAMECLIENT.IsReplayModeOn())
+        GAMECLIENT.ToggleHumanAIPlayer(AI::Info{AI::Type::Default, AI::Level::Easy});
 }
 
 void Cheats::armageddon()
 {
-    if(!isCheatModeOn())
-        return;
-
-    GAMECLIENT.CheatArmageddon();
+    if(isCheatModeOn())
+        GAMECLIENT.CheatArmageddon();
 }
 
-bool Cheats::canCheatModeBeOn() const
+bool Cheats::areCheatsAllowed() const
 {
     return world_.IsSinglePlayer();
 }
