ae0cc0899704450c7b46b775b6038541d24a53b9
Alexander Grund <Flamefire@users.noreply.github.com>
2024-10-13 19:01:00 +0200

    Add jump-to-position to Map-Debug window
    For debugging we often want to view a specific position.
    Make this easy by specifying the position as X-Y and Go-To with RETURN.
    TAB can be used to change between the edits which also clears the newly
    activated one such that new coordinates can be entered.

--- libs/s25main/ingameWindows/iwMapDebug.h
+++ libs/s25main/ingameWindows/iwMapDebug.h
@@ -22,6 +22,10 @@ private:
     void Msg_ComboSelectItem(unsigned ctrl_id, unsigned select) override;
     void Msg_CheckboxChange(unsigned ctrl_id, bool checked) override;
     void Msg_Timer(unsigned ctrl_id) override;
+    void Msg_EditEnter(unsigned ctrl_id) override;
+    bool Msg_KeyDown(const KeyEvent& ke);
+
+    void SetActive(bool activate) override;
 
     GameWorldView& gwv;
     std::unique_ptr<DebugPrinter> printer;
