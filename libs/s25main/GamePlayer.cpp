ce7376b2009c3a4cc17714497390b20735620001
Sunrise <schafli@wolke7.net>
2024-12-01 19:22:22 +0100

    Add direct transport route

--- libs/s25main/GamePlayer.cpp
+++ libs/s25main/GamePlayer.cpp
@@ -139,6 +139,8 @@ void GamePlayer::LoadStandardDistribution()
     distribution[GoodType::Stones].client_buildings.push_back(BuildingType::Catapult);
     distribution[GoodType::Grapes].client_buildings.push_back(BuildingType::Winery);
     distribution[GoodType::Wine].client_buildings.push_back(BuildingType::Temple);
+    distribution[GoodType::Skins].client_buildings.push_back(BuildingType::Tannery);
+    distribution[GoodType::Leather].client_buildings.push_back(BuildingType::LeatherWorks);
 
     // Waren mit mehreren möglichen Zielen erstmal nullen, kann dann im Fenster eingestellt werden
     for(const auto i : helpers::enumRange<GoodType>())
