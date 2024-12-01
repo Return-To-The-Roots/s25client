8d6e01cb6734f9e95a1ad6630f64f39ec6ba6fa7
Sunrise <schafli@wolke7.net>
2024-12-01 17:13:58 +0100

    Adding ware workflow, buildings and menus for leather addon

---
+++ libs/s25main/LeatherLoader.cpp
@@ -0,0 +1,89 @@
+// Copyright (C) 2024 Settlers Freaks (sf-team at siedler25.org)
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+///////////////////////////////////////////////////////////////////////////////
+
+#include "LeatherLoader.h"
+#include "GlobalGameSettings.h"
+#include "Loader.h"
+#include "addons/const_addons.h"
+#include "ogl/glArchivItem_Bitmap.h"
+#include "ogl/glTexturePacker.h"
+#include "world/GameWorldBase.h"
+
+namespace leatheraddon {
+
+bool isLeatherAddonBuildingType(BuildingType bld)
+{
+    return bld == BuildingType::Skinner || bld == BuildingType::Tannery || bld == BuildingType::LeatherWorks;
+}
+
+bool isLeatherAddonGoodType(GoodType good)
+{
+    return good == GoodType::Skins || good == GoodType::Leather || good == GoodType::Armor;
+}
+
+bool isLeatherAddonJobType(Job job)
+{
+    return job == Job::Skinner || job == Job::Tanner || job == Job::LeatherWorker;
+}
+
+helpers::EnumArray<unsigned, BobTypes> bobIndex = {0,   21,  69,  117, 125, 173, 189, 222, 270, 286, 293,
+                                                   301, 307, 313, 336, 354, 355, 356, 356, 357, 357, 358,
+                                                   359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369};
+
+ITexture* GetWareTex(const GoodType good)
+{
+    switch(good)
+    {
+        case GoodType::Skins: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::SKINS_WARE_ICON]);
+        case GoodType::Leather: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::LEATHER_WARE_ICON]);
+        case GoodType::Armor: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::ARMOR_WARE_ICON]);
+        default: return nullptr;
+    }
+}
+
+ITexture* GetWareStackTex(const GoodType good)
+{
+    switch(good)
+    {
+        case GoodType::Skins: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::SKINS_WARE_ON_GROUND_OF_FLAG]);
+        case GoodType::Leather:
+            return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::LEATHER_WARE_ON_GROUND_OF_FLAG]);
+        case GoodType::Armor: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::ARMOR_WARE_ON_GROUND_OF_FLAG]);
+        default: return nullptr;
+    }
+}
+
+ITexture* GetWareDonkeyTex(const GoodType good)
+{
+    switch(good)
+    {
+        case GoodType::Skins:
+            return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::DONKEY_BOAT_CARRYING_SKINS_WARE]);
+        case GoodType::Leather:
+            return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::DONKEY_BOAT_CARRYING_LEATHER_WARE]);
+        case GoodType::Armor:
+            return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::DONKEY_BOAT_CARRYING_ARMOR_WARE]);
+        default: return nullptr;
+    }
+}
+
+ITexture* GetJobTex(Job job)
+{
+    switch(job)
+    {
+        case Job::Skinner: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::SKINNER_JOB_ICON]);
+        case Job::Tanner: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::TANNER_JOB_ICON]);
+        case Job::LeatherWorker: return LOADER.GetImageN("leather_bobs", bobIndex[BobTypes::LEATHERWORKER_JOB_ICON]);
+        default: return nullptr;
+    }
+}
+
+bool isAddonActive(const GameWorldBase& gwb)
+{
+    return gwb.GetGGS().isEnabled(AddonId::LEATHER);
+}
+
+} // namespace leatheraddon
---
+++ libs/s25main/figures/nofSkinner.h
@@ -0,0 +1,33 @@
+// Copyright (C) 2024 Settlers Freaks (sf-team at siedler25.org)
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "nofWorkman.h"
+
+class SerializedGameData;
+class nobUsual;
+
+#ifdef _MSC_VER
+#    pragma warning(disable : 4646) // function declared with [[noreturn]] has non-void return type
+#endif
+
+class nofSkinner : public nofWorkman
+{
+    /// Draw worker at work
+    void DrawWorking(DrawPoint drawPt) override;
+    /// Id in jobs.bob or carrier.bob when carrying a ware
+    [[noreturn]] unsigned short GetCarryID() const override;
+    /// Der Arbeiter erzeugt eine Ware
+    helpers::OptionalEnum<GoodType> ProduceWare() override;
+
+    /// Draws the figure while returning home / entering the building (often carrying wares)
+    void DrawWalkingWithWare(DrawPoint drawPt) override;
+
+public:
+    nofSkinner(MapPoint pos, unsigned char player, nobUsual* workplace);
+    nofSkinner(SerializedGameData& sgd, unsigned obj_id);
+
+    GO_Type GetGOT() const final { return GO_Type::NofSkinner; }
+};
