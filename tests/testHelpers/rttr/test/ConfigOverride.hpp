37b6447aecdba90f46ab4fbf71e9d017d9e7ddac
Alexander Grund <Flamefire@users.noreply.github.com>
2025-04-19 11:13:38 +0200

    Add class for scoped RTTRConfig overriding
    Make existing fixture implementation reusable.

---
+++ tests/testHelpers/rttr/test/ConfigOverride.hpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2005 - 2025 Settlers Freaks (sf-team at siedler25.org)
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "RttrConfig.h"
+
+namespace rttr::test {
+/// Temporarily change a RTTRCONFIG path
+class ConfigOverride
+{
+    const std::string entry;
+    boost::filesystem::path oldPath;
+
+public:
+    ConfigOverride(std::string pathEntry, const boost::filesystem::path& newPath)
+        : entry(std::move(pathEntry)), oldPath(RTTRCONFIG.ExpandPath("<RTTR_" + entry + ">"))
+    {
+        RTTRCONFIG.overridePathMapping(entry, newPath);
+    }
+    ~ConfigOverride() { RTTRCONFIG.overridePathMapping(entry, std::move(oldPath)); }
+};
+} // namespace rttr::test
