22eff1ccd995af844cb3e82b42347d7a90b9a08a
Sunrise <schafli@wolke7.net>
2025-06-09 11:56:14 +0200

    Remove code duplication and draw +1 only if HITPOINTS addon is activated. Show armor icon above soldier if he has armor but not for soliders in the inventory of the ship

--- libs/s25main/figures/nofArmored.cpp
+++ libs/s25main/figures/nofArmored.cpp
@@ -37,10 +37,18 @@ nofArmored::nofArmored(SerializedGameData& sgd, const unsigned obj_id) : noFigur
 
 void nofArmored::DrawArmorWalking(DrawPoint drawPt)
 {
-    if(HasArmor() && world->GetGGS().isEnabled(AddonId::MILITARY_HITPOINTS))
+    DrawArmor(drawPt, CalcFigurRelative());
+}
+
+void nofArmored::DrawArmor(DrawPoint drawPt, DrawPoint offset)
+{
+    if(HasArmor())
     {
-        SmallFont->Draw(drawPt + DrawPoint(7, -20) + CalcFigurRelative(), "+", FontStyle::CENTER, COLOR_RED);
-        SmallFont->Draw(drawPt + DrawPoint(10, -20) + CalcFigurRelative(), "1", FontStyle::CENTER, COLOR_RED);
+        if(world->GetGGS().isEnabled(AddonId::MILITARY_HITPOINTS))
+        {
+            SmallFont->Draw(drawPt + DrawPoint(7, -20) + CalcFigurRelative(), "+", FontStyle::CENTER, COLOR_RED);
+            SmallFont->Draw(drawPt + DrawPoint(10, -20) + CalcFigurRelative(), "1", FontStyle::CENTER, COLOR_RED);
+        }
 
         LOADER
           .GetImageN("leather_bobs", leatheraddon::bobIndex[leatheraddon::BobTypes::DONKEY_BOAT_CARRYING_ARMOR_WARE])
@@ -50,15 +58,7 @@ void nofArmored::DrawArmorWalking(DrawPoint drawPt)
 
 void nofArmored::DrawArmorNotWalking(DrawPoint drawPt)
 {
-    if(HasArmor() && world->GetGGS().isEnabled(AddonId::MILITARY_HITPOINTS))
-    {
-        SmallFont->Draw(drawPt + DrawPoint(7, -20), "+", FontStyle::CENTER, COLOR_RED);
-        SmallFont->Draw(drawPt + DrawPoint(10, -20), "1", FontStyle::CENTER, COLOR_RED);
-
-        LOADER
-          .GetImageN("leather_bobs", leatheraddon::bobIndex[leatheraddon::BobTypes::DONKEY_BOAT_CARRYING_ARMOR_WARE])
-          ->DrawFull(drawPt + DrawPoint(0, -22));
-    }
+    DrawArmorWalking(drawPt);
 }
 
 bool nofArmored::HasArmor() const
--- libs/s25main/figures/nofArmored.h
+++ libs/s25main/figures/nofArmored.h
@@ -18,6 +18,8 @@ protected:
 
     explicit nofArmored(const nofArmored&) = default;
 
+    void DrawArmor(DrawPoint drawPt, DrawPoint offset = {0, 0});
+
 public:
     void DrawArmorWalking(DrawPoint drawPt);
     void DrawArmorNotWalking(DrawPoint drawPt);
