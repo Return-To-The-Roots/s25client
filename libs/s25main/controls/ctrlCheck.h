e8e617576783b31dce253b16c38ae077e294184d
Alexander Grund <Flamefire@users.noreply.github.com>
2025-08-24 14:08:28 +0200

    Support tooltips for checkboxes

--- libs/s25main/controls/ctrlCheck.h
+++ libs/s25main/controls/ctrlCheck.h
@@ -5,21 +5,34 @@
 #pragma once
 
 #include "Window.h"
+#include "ctrlBaseTooltip.h"
+#include <string>
 class MouseCoords;
 class glFont;
 
-class ctrlCheck : public Window
+class ctrlCheck : public Window, ctrlBaseTooltip
 {
 public:
     ctrlCheck(Window* parent, unsigned id, const DrawPoint& pos, const Extent& size, TextureColor tc, std::string text,
               const glFont* font, bool readonly);
 
-    void setChecked(bool checked) { this->check = checked; }
+    ctrlCheck* setChecked(bool checked)
+    {
+        this->check = checked;
+        return this;
+    }
     bool isChecked() const { return check; }
-    void setReadOnly(bool readonly) { this->readonly = readonly; }
+    ctrlCheck* setReadOnly(bool readonly)
+    {
+        this->readonly = readonly;
+        return this;
+    }
     bool isReadOnly() const { return readonly; }
 
+    ctrlCheck* setTooltip(std::string tooltip);
+
     bool Msg_LeftDown(const MouseCoords& mc) override;
+    bool Msg_MouseMove(const MouseCoords& mc) override;
 
 protected:
     void Draw_() override;
