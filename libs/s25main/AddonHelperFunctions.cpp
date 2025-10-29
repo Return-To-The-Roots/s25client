a06b72756e5cac1056b28d192cf2a79b0b572bb7
Sunrise <schafli@wolke7.net>
2025-10-29 16:58:05 +0100

    Code review: Move addon check functions for unused Ware/Jobs/Buildings to separate header to a void duplication

---
+++ libs/s25main/AddonHelperFunctions.cpp
@@ -0,0 +1,47 @@
+// Copyright (C) 2005 - 2025 Settlers Freaks (sf-team at siedler25.org)
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include "AddonHelperFunctions.h"
+#include "GlobalGameSettings.h"
+#include "world/GameWorld.h"
+#include <LeatherLoader.h>
+#include <WineLoader.h>
+#include <addons/Addon.h>
+
+auto isUnusedBuilding(GamePlayer const& player) -> std::function<bool(BuildingType const& type)>
+{
+    return [&player = player](BuildingType const& bld) {
+        if(!wineaddon::isAddonActive(player.GetGameWorld()) && wineaddon::isWineAddonBuildingType(bld))
+            return true;
+        if(!player.GetGameWorld().GetGGS().isEnabled(AddonId::CHARBURNER) && bld == BuildingType::Charburner)
+            return true;
+        if(!leatheraddon::isAddonActive(player.GetGameWorld()) && leatheraddon::isLeatherAddonBuildingType(bld))
+            return true;
+        return false;
+    };
+}
+
+auto isUnusedWare(GamePlayer const& player) -> std::function<bool(GoodType const& type)>
+{
+    return [&player = player](GoodType const& type) {
+        if(!wineaddon::isAddonActive(player.GetGameWorld()) && wineaddon::isWineAddonGoodType(type))
+            return true;
+        if(!leatheraddon::isAddonActive(player.GetGameWorld()) && leatheraddon::isLeatherAddonGoodType(type))
+            return true;
+        return false;
+    };
+}
+
+auto isUnusedJob(GamePlayer const& player) -> std::function<bool(Job const& job)>
+{
+    return [&](Job const& job) {
+        if(!wineaddon::isAddonActive(player.GetGameWorld()) && wineaddon::isWineAddonJobType(job))
+            return true;
+        if(!player.GetGameWorld().GetGGS().isEnabled(AddonId::CHARBURNER) && job == Job::CharBurner)
+            return true;
+        if(!leatheraddon::isAddonActive(player.GetGameWorld()) && leatheraddon::isLeatherAddonJobType(job))
+            return true;
+        return false;
+    };
+}
--- libs/s25main/AddonHelperFunctions.h
+++ libs/s25main/AddonHelperFunctions.h
@@ -5,46 +5,7 @@
 #pragma once
 
 #include "GamePlayer.h"
-#include "GlobalGameSettings.h"
-#include "world/GameWorld.h"
-#include <LeatherLoader.h>
-#include <WineLoader.h>
-#include <addons/Addon.h>
-#include <gameData/BuildingConsts.h>
 
-auto isUnusedBuilding(GamePlayer const& player)
-{
-    return [&player = player](BuildingType const& bld) {
-        if(!wineaddon::isAddonActive(player.GetGameWorld()) && wineaddon::isWineAddonBuildingType(bld))
-            return true;
-        if(!player.GetGameWorld().GetGGS().isEnabled(AddonId::CHARBURNER) && bld == BuildingType::Charburner)
-            return true;
-        if(!leatheraddon::isAddonActive(player.GetGameWorld()) && leatheraddon::isLeatherAddonBuildingType(bld))
-            return true;
-        return false;
-    };
-}
-
-auto isUnusedWare(GamePlayer const& player)
-{
-    return [&player = player](GoodType const& type) {
-        if(!wineaddon::isAddonActive(player.GetGameWorld()) && wineaddon::isWineAddonGoodType(type))
-            return true;
-        if(!leatheraddon::isAddonActive(player.GetGameWorld()) && leatheraddon::isLeatherAddonGoodType(type))
-            return true;
-        return false;
-    };
-}
-
-auto isUnusedJob(GamePlayer const& player)
-{
-    return [&](Job const& job) {
-        if(!wineaddon::isAddonActive(player.GetGameWorld()) && wineaddon::isWineAddonJobType(job))
-            return true;
-        if(!player.GetGameWorld().GetGGS().isEnabled(AddonId::CHARBURNER) && job == Job::CharBurner)
-            return true;
-        if(!leatheraddon::isAddonActive(player.GetGameWorld()) && leatheraddon::isLeatherAddonJobType(job))
-            return true;
-        return false;
-    };
-}
+auto isUnusedBuilding(GamePlayer const& player) -> std::function<bool(BuildingType const& type)>;
+auto isUnusedWare(GamePlayer const& player) -> std::function<bool(GoodType const& type)>;
+auto isUnusedJob(GamePlayer const& player) -> std::function<bool(Job const& job)>;
