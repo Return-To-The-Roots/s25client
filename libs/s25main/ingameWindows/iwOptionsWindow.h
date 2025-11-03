d65b9c64f2a100d2e404c554e257216d94c2424c
Mario Okrogelnik <bugs.okromario@gmx.at>
2025-11-03 18:49:54 +0100

    Add option to enable/disable bird sounds

--- libs/s25main/ingameWindows/iwOptionsWindow.h
+++ libs/s25main/ingameWindows/iwOptionsWindow.h
@@ -18,4 +18,5 @@ public:
 private:
     void Msg_ButtonClick(unsigned ctrl_id) override;
     void Msg_ProgressChange(unsigned ctrl_id, unsigned short position) override;
+    void Msg_CheckboxChange(unsigned ctrl_id, bool checked) override;
 };
