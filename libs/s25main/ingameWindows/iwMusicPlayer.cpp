87056ae9d4aa3dc516532156bc5364a3ce42d5a6
Alexander Grund <Flamefire@users.noreply.github.com>
2025-11-23 19:25:49 +0100

    Fix song selection in music player window
    Selecting a start song when the playlist isn't prepared yet isn't
    possible because the `order` list isn't filled and hence the selected
    song isn't found.
    
    Make sure we prepare it first and add test for that.
    
    Fixes #1833
    Update the translations, fixes #1832

--- libs/s25main/ingameWindows/iwMusicPlayer.cpp
+++ libs/s25main/ingameWindows/iwMusicPlayer.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2005 - 2021 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (C) 2005 - 2025 Settlers Freaks (sf-team at siedler25.org)
 //
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -86,7 +86,7 @@ iwMusicPlayer::iwMusicPlayer()
     AddText(ID_txtPlaylist, DrawPoint(20, 240), _("Playlist:"), COLOR_YELLOW, FontStyle{}, NormalFont);
     AddComboBox(ID_cbPlaylist, DrawPoint(20, 260), Extent(330, 22), TextureColor::Green1, NormalFont, 200);
 
-    // Playlistbuttons
+    // Playlist buttons
     const unsigned short button_distance = 10;
     const Extent buttonSize((330 - button_distance) / 2, 22);
     ctrlButton* b1 =
@@ -94,7 +94,7 @@ iwMusicPlayer::iwMusicPlayer()
     AddTextButton(ID_btRemovePlaylist, b1->GetPos() + DrawPoint(buttonSize.x + button_distance, 0), buttonSize,
                   TextureColor::Green2, _("Remove"), NormalFont);
 
-    // Buttons für die Musikstücke
+    // Track buttons
     AddImageButton(ID_btAddTrack, DrawPoint(360, 30), Extent(30, 40), TextureColor::Grey, LOADER.GetImageN("io", 138),
                    _("Add track"));
     AddImageButton(ID_btAddTrackDir, DrawPoint(390, 30), Extent(30, 40), TextureColor::Grey,
@@ -116,7 +116,7 @@ iwMusicPlayer::iwMusicPlayer()
     AddImageButton(ID_btSave, DrawPoint(370, 270), Extent(40, 40), TextureColor::Grey, LOADER.GetImageN("io", 37),
                    _("Save playlist"));
 
-    // Mit Werten füllen
+    // Fill values for current playlist
     UpdateFromPlaylist(MUSICPLAYER.GetPlaylist());
     UpdatePlaylistCombo(SETTINGS.sound.playlist);
     auto* cbPlayList = GetCtrl<ctrlComboBox>(ID_cbPlaylist);
@@ -259,7 +259,7 @@ void iwMusicPlayer::Msg_ButtonClick(const unsigned ctrl_id)
             {
                 const std::string playlistName(GetCtrl<ctrlComboBox>(ID_cbPlaylist)->GetText(*selection));
 
-                // RTTR-Playlisten dürfen nicht gelöscht werden
+                // RTTR-playlists must not be deleted
                 if(isReadonlyPlaylist(playlistName))
                 {
                     WINDOWMANAGER.Show(
@@ -355,7 +355,7 @@ void iwMusicPlayer::Msg_Input(const unsigned win_id, const std::string& msg)
                 valid = true;
             else
             {
-                // Evtl ein Siedlerstück ("sNN")?
+                // Could be an original The settlers song ("sNN")?
                 if(msg.length() == 3 && msg[0] == 's')
                 {
                     if(s25util::fromStringClassicDef(msg.substr(1), 999u) <= 14u)
@@ -363,10 +363,8 @@ void iwMusicPlayer::Msg_Input(const unsigned win_id, const std::string& msg)
                 }
             }
 
-            // Gültiges Siedlerstück?
             if(valid)
             {
-                // Hinzufügen
                 GetCtrl<ctrlList>(ID_lstSongs)->AddString(msg);
                 changed = true;
             } else
@@ -376,17 +374,15 @@ void iwMusicPlayer::Msg_Input(const unsigned win_id, const std::string& msg)
         break;
         case ID_wndAddPlaylist:
         {
-            // Ungültige Namen ausschließen
+            // Playlist name must start with a letter
             const auto isLetter = [](const char c) { return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'); };
             const boost::filesystem::path fullPlaylistPath = GetFullPlaylistPath(msg);
             if(!msg.empty() && isLetter(msg.front()) && Playlist().SaveAs(fullPlaylistPath))
             {
-                // Combobox updaten
                 UpdatePlaylistCombo(fullPlaylistPath.string());
                 changed = true;
             } else
             {
-                // Fehler, konnte nicht gespeichert werden
                 WINDOWMANAGER.Show(std::make_unique<iwMsgbox>(_("Error"), _("The specified file couldn't be saved!"),
                                                               this, MsgboxButton::Ok, MsgboxIcon::ExclamationRed));
             }
@@ -428,7 +424,6 @@ void iwMusicPlayer::SetRandomPlayback(const bool random_playback)
       ->SetTooltip(random_playback ? _("Playback in this order") : _("Random playback"));
 }
 
-/// Updatet die Playlist - Combo
 void iwMusicPlayer::UpdatePlaylistCombo(const std::string& highlight_entry)
 {
     auto* cbPlaylist = GetCtrl<ctrlComboBox>(ID_cbPlaylist);
