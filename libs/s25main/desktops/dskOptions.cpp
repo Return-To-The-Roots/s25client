832bc2bb34e5e52baff73f22081c45e7d3f2e8ca
Alexander Grund <Flamefire@users.noreply.github.com>
2026-02-22 20:46:57 +0100

    Refactor control creation of dskOptions

--- libs/s25main/desktops/dskOptions.cpp
+++ libs/s25main/desktops/dskOptions.cpp
@@ -14,6 +14,7 @@
 #include "controls/ctrlImageButton.h"
 #include "controls/ctrlOptionGroup.h"
 #include "controls/ctrlProgress.h"
+#include "controls/ctrlTextButton.h"
 #include "driver/VideoDriver.h"
 #include "drivers/AudioDriverWrapper.h"
 #include "drivers/VideoDriverWrapper.h"
@@ -66,15 +67,11 @@ enum
     ID_cbProxyType,
     ID_txtDebugData,
     ID_grpDebugData,
-    ID_txtUPNP,
     ID_grpUPNP,
     ID_txtMapScrollMode,
     ID_cbMapScrollMode,
-    ID_txtSmartCursor,
     ID_grpSmartCursor,
-    ID_txtWindowPinning,
     ID_grpWindowPinning,
-    ID_txtGFInfo,
     ID_grpGFInfo,
     ID_txtResolution,
     ID_cbResolution,
@@ -82,28 +79,24 @@ enum
     ID_cbDisplayMode,
     ID_txtFramerate,
     ID_cbFramerate,
-    ID_txtVBO,
     ID_grpVBO,
     ID_txtVideoDriver,
     ID_cbVideoDriver,
-    ID_txtOptTextures,
     ID_grpOptTextures,
     ID_txtGuiScale,
     ID_cbGuiScale,
     ID_txtAudioDriver,
     ID_cbAudioDriver,
-    ID_txtMusic,
     ID_grpMusic,
     ID_pgMusicVol,
-    ID_txtEffects,
     ID_grpEffects,
     ID_pgEffectsVol,
     ID_btMusicPlayer,
     ID_txtCommonPortrait,
     ID_btCommonPortrait,
     ID_cbCommonPortrait,
-    ID_txtBirdSounds,
     ID_grpBirdSounds,
+    ID_txtsStart, // First ID for texts used by options (ID of option is used as offset on top)
 };
 // Use these as IDs in dedicated groups
 constexpr auto ID_btOn = 1;
@@ -122,9 +115,8 @@ constexpr Offset ctrlOffset(200, -5);                       // Offset of control
 constexpr Offset ctrlOffset2 = ctrlOffset + Offset(200, 0); // Offset of 2nd control to its description text
 constexpr Extent ctrlSize(190, 22);
 constexpr Extent ctrlSizeLarge = ctrlSize + Extent(ctrlOffset2 - ctrlOffset);
-} // namespace
 
-static VideoMode getAspectRatio(const VideoMode& vm)
+VideoMode getAspectRatio(const VideoMode& vm)
 {
     // First some a bit off values where the aspect ratio is defined by convention
     if(vm == VideoMode(1360, 1024))
@@ -143,6 +135,32 @@ static VideoMode getAspectRatio(const VideoMode& vm)
     else
         return ratio;
 }
+
+enum class ButtonSize
+{
+    Normal,
+    Half /// Both buttons take up the same space as a single button
+};
+
+ctrlOptionGroup& addOnOffOption(ctrlGroup& parentGroup, DrawPoint pos, unsigned id, const std::string& title,
+                                bool value, ButtonSize btSize = ButtonSize::Normal)
+{
+    constexpr auto spacing = 10;
+    auto size = ctrlSize;
+    if(btSize == ButtonSize::Half)
+        size.x = (ctrlSize.x - spacing) / 2;
+
+    parentGroup.AddText(ID_txtsStart + id, pos, title, COLOR_YELLOW, FontStyle{}, NormalFont);
+    auto* option = parentGroup.AddOptionGroup(id, GroupSelectType::Check);
+    pos += ctrlOffset;
+    option->AddTextButton(ID_btOn, pos, size, TextureColor::Grey, _("On"), NormalFont);
+    pos.x += size.x + spacing;
+    option->AddTextButton(ID_btOff, pos, size, TextureColor::Grey, _("Off"), NormalFont);
+    option->SetSelection(value);
+    return *option;
+}
+
+} // namespace
 
 dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
 {
@@ -229,14 +247,11 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     curPos.y += rowHeight;
 
     // IPv4/6
-    groupCommon->AddText(ID_txtIpv6, curPos, _("Use IPv6:"), COLOR_YELLOW, FontStyle{}, NormalFont);
-
-    ctrlOptionGroup* ipv6 = groupCommon->AddOptionGroup(ID_grpIpv6, GroupSelectType::Check);
-    ipv6->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("IPv6"), NormalFont);
-    ipv6->AddTextButton(ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("IPv4"), NormalFont);
-    ipv6->SetSelection(SETTINGS.server.ipv6);
+    auto& grpIPv6 = addOnOffOption(*groupCommon, curPos, ID_grpIpv6, _("Use IPv6:"), SETTINGS.server.ipv6);
+    grpIPv6.GetCtrl<ctrlTextButton>(ID_btOn)->SetText(_("IPv6"));
+    grpIPv6.GetCtrl<ctrlTextButton>(ID_btOff)->SetText(_("IPv4"));
     // Enable/disable the IPv6 field if necessary
-    ipv6->GetCtrl<ctrlButton>(1)->SetEnabled(SETTINGS.proxy.type != ProxyType::Socks5); //-V807
+    grpIPv6.GetCtrl<ctrlButton>(ID_btOn)->SetEnabled(SETTINGS.proxy.type != ProxyType::Socks5); //-V807
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacingCommon;
@@ -250,11 +265,7 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     proxy->SetText(SETTINGS.proxy.port);
     curPos.y += rowHeight;
 
-    groupCommon->AddText(ID_txtUPNP, curPos, _("Use UPnP"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    ctrlOptionGroup* upnp = groupCommon->AddOptionGroup(ID_grpUPNP, GroupSelectType::Check);
-    upnp->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("On"), NormalFont);
-    upnp->AddTextButton(ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("Off"), NormalFont);
-    upnp->SetSelection(SETTINGS.global.useUPNP);
+    addOnOffOption(*groupCommon, curPos, ID_grpUPNP, _("Use UPnP"), SETTINGS.global.useUPNP);
     curPos.y += rowHeight;
 
     // Proxy type
@@ -285,23 +296,19 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     combo->SetSelection(static_cast<int>(SETTINGS.interface.mapScrollMode));
     curPos.y += rowHeight;
 
-    groupCommon->AddText(ID_txtSmartCursor, curPos, _("Smart Cursor"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    ctrlOptionGroup* smartCursor = groupCommon->AddOptionGroup(ID_grpSmartCursor, GroupSelectType::Check);
-    smartCursor->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("On"), NormalFont,
-                               _("Place cursor on default button for new dialogs / action windows (default)"));
-    smartCursor->AddTextButton(
-      ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("Off"), NormalFont,
+    auto& grpSmartCursor =
+      addOnOffOption(*groupCommon, curPos, ID_grpSmartCursor, _("Smart Cursor"), SETTINGS.global.smartCursor);
+    grpSmartCursor.GetCtrl<ctrlButton>(ID_btOn)->SetTooltip(
+      _("Place cursor on default button for new dialogs / action windows (default)"));
+    grpSmartCursor.GetCtrl<ctrlButton>(ID_btOff)->SetTooltip(
       _("Don't move cursor automatically\nUseful e.g. for split-screen / dual-mice multiplayer (see wiki)"));
-    smartCursor->SetSelection(SETTINGS.global.smartCursor);
     curPos.y += rowHeight;
 
-    groupCommon->AddText(ID_txtWindowPinning, curPos, _("Window pinning"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    ctrlOptionGroup* windowPinning = groupCommon->AddOptionGroup(ID_grpWindowPinning, GroupSelectType::Check);
-    windowPinning->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("On"), NormalFont,
-                                 _("Replace minimize button on windows by pin button avoiding closing the window with "
-                                   "ESC.\nMinimize by double-clicking the title bar."));
-    windowPinning->AddTextButton(ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("Off"), NormalFont);
-    windowPinning->SetSelection(SETTINGS.interface.enableWindowPinning);
+    auto& grpWindowPinning = addOnOffOption(*groupCommon, curPos, ID_grpWindowPinning, _("Window pinning"),
+                                            SETTINGS.interface.enableWindowPinning);
+    grpWindowPinning.GetCtrl<ctrlButton>(ID_btOn)->SetTooltip(
+      _("Replace minimize button on windows by pin button avoiding closing the window with "
+        "ESC.\nMinimize by double-clicking the title bar."));
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacingCommon;
@@ -315,12 +322,7 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     mainGroup->SetSelection((SETTINGS.global.submitDebugData == 1) ? ID_btSubmitDebugOn : ID_btSubmitDebugAsk); //-V807
     curPos.y += rowHeight;
 
-    groupCommon->AddText(ID_txtGFInfo, curPos, _("Show GameFrame Info:"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    mainGroup = groupCommon->AddOptionGroup(ID_grpGFInfo, GroupSelectType::Check);
-    mainGroup->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("On"), NormalFont);
-    mainGroup->AddTextButton(ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("Off"), NormalFont);
-
-    mainGroup->SetSelection(SETTINGS.global.showGFInfo);
+    addOnOffOption(*groupCommon, curPos, ID_grpGFInfo, _("Show GameFrame Info:"), SETTINGS.global.showGFInfo);
 
     curPos = optionRowsStartPosition;
     groupGraphics->AddText(ID_txtResolution, curPos, _("Fullscreen resolution:"), COLOR_YELLOW, FontStyle{},
@@ -344,10 +346,7 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacing;
-    groupGraphics->AddText(ID_txtVBO, curPos, _("Vertex Buffer Objects:"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    mainGroup = groupGraphics->AddOptionGroup(ID_grpVBO, GroupSelectType::Check);
-    mainGroup->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("On"), NormalFont);
-    mainGroup->AddTextButton(ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("Off"), NormalFont);
+    addOnOffOption(*groupGraphics, curPos, ID_grpVBO, _("Vertex Buffer Objects:"), SETTINGS.video.vbo);
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacing;
@@ -366,11 +365,7 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacing;
-    groupGraphics->AddText(ID_txtOptTextures, curPos, _("Optimized Textures:"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    mainGroup = groupGraphics->AddOptionGroup(ID_grpOptTextures, GroupSelectType::Check);
-
-    mainGroup->AddTextButton(ID_btOn, curPos + ctrlOffset, ctrlSize, TextureColor::Grey, _("On"), NormalFont);
-    mainGroup->AddTextButton(ID_btOff, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, _("Off"), NormalFont);
+    addOnOffOption(*groupGraphics, curPos, ID_grpOptTextures, _("Optimized Textures:"), SETTINGS.video.sharedTextures);
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacing;
@@ -379,36 +374,24 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     updateGuiScale();
 
     curPos = optionRowsStartPosition;
-    constexpr Offset bt1Offset(200, -5);
-    constexpr Offset bt2Offset(300, -5);
-    constexpr Offset volOffset(400, -5);
-    constexpr Extent ctrlSizeSmall(90, ctrlSize.y);
 
-    groupSound->AddText(ID_txtEffects, curPos, _("Effects"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    mainGroup = groupSound->AddOptionGroup(ID_grpEffects, GroupSelectType::Check);
-    mainGroup->AddTextButton(ID_btOn, curPos + bt1Offset, ctrlSizeSmall, TextureColor::Grey, _("On"), NormalFont);
-    mainGroup->AddTextButton(ID_btOff, curPos + bt2Offset, ctrlSizeSmall, TextureColor::Grey, _("Off"), NormalFont);
+    addOnOffOption(*groupSound, curPos, ID_grpEffects, _("Effects"), SETTINGS.sound.effectsEnabled, ButtonSize::Half);
 
     ctrlProgress* FXvolume =
-      groupSound->AddProgress(ID_pgEffectsVol, curPos + volOffset, ctrlSize, TextureColor::Grey, 139, 138, 100);
+      groupSound->AddProgress(ID_pgEffectsVol, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, 139, 138, 100);
     FXvolume->SetPosition((SETTINGS.sound.effectsVolume * 100) / 255);
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacing;
-    groupSound->AddText(ID_txtBirdSounds, curPos, _("Bird sounds"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    mainGroup = groupSound->AddOptionGroup(ID_grpBirdSounds, GroupSelectType::Check);
-    mainGroup->AddTextButton(ID_btOn, curPos + bt1Offset, ctrlSizeSmall, TextureColor::Grey, _("On"), NormalFont);
-    mainGroup->AddTextButton(ID_btOff, curPos + bt2Offset, ctrlSizeSmall, TextureColor::Grey, _("Off"), NormalFont);
+    addOnOffOption(*groupSound, curPos, ID_grpBirdSounds, _("Bird sounds"), SETTINGS.sound.birdsEnabled,
+                   ButtonSize::Half);
     curPos.y += rowHeight;
 
     curPos.y += sectionSpacing;
-    groupSound->AddText(ID_txtMusic, curPos, _("Music"), COLOR_YELLOW, FontStyle{}, NormalFont);
-    mainGroup = groupSound->AddOptionGroup(ID_grpMusic, GroupSelectType::Check);
-    mainGroup->AddTextButton(ID_btOn, curPos + bt1Offset, ctrlSizeSmall, TextureColor::Grey, _("On"), NormalFont);
-    mainGroup->AddTextButton(ID_btOff, curPos + bt2Offset, ctrlSizeSmall, TextureColor::Grey, _("Off"), NormalFont);
+    addOnOffOption(*groupSound, curPos, ID_grpMusic, _("Music"), SETTINGS.sound.musicEnabled, ButtonSize::Half);
 
     ctrlProgress* Mvolume =
-      groupSound->AddProgress(ID_pgMusicVol, curPos + volOffset, ctrlSize, TextureColor::Grey, 139, 138, 100);
+      groupSound->AddProgress(ID_pgMusicVol, curPos + ctrlOffset2, ctrlSize, TextureColor::Grey, 139, 138, 100);
     Mvolume->SetPosition((SETTINGS.sound.musicVolume * 100) / 255); //-V807
     curPos.y += rowHeight;
 
@@ -476,9 +459,6 @@ dskOptions::dskOptions() : Desktop(LOADER.GetImageN("setup013", 0))
     if(!cbFrameRate->GetSelection())
         cbFrameRate->SetSelection(0);
 
-    groupGraphics->GetCtrl<ctrlOptionGroup>(ID_grpVBO)->SetSelection(SETTINGS.video.vbo);
-
-    groupGraphics->GetCtrl<ctrlOptionGroup>(ID_grpOptTextures)->SetSelection(SETTINGS.video.sharedTextures);
     // }
 
     // Sound
@@ -614,6 +594,7 @@ void dskOptions::Msg_Group_OptionGroupChange(const unsigned /*group_id*/, const 
             break;
         case ID_grpWindowPinning: SETTINGS.interface.enableWindowPinning = enabled; break;
         case ID_grpGFInfo: SETTINGS.global.showGFInfo = enabled; break;
+        default: RTTR_Assert(false);
     }
 }
 
