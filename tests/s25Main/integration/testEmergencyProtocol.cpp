0c79309a84adb05d6eeeb40073a390b4b5cbdf7f
lukas <tost>
2026-07-18 14:31:50 +0200

    formated test file

--- tests/s25Main/integration/testEmergencyProtocol.cpp
+++ tests/s25Main/integration/testEmergencyProtocol.cpp
@@ -15,12 +15,12 @@
 #include "worldFixtures/initGameRNG.hpp"
 #include "gameTypes/GO_Type.h"
 #include <boost/test/unit_test.hpp>
-#include <buildings/nobUsual.h>
 #include <buildings/nobHQ.h>
+#include <buildings/nobUsual.h>
 
 struct EmergencyFixture : public WorldFixture<CreateEmptyWorld, 1>
 {
-    nobHQ * HQ = world.GetPlayer(0).GetHQ();
+    nobHQ* HQ = world.GetPlayer(0).GetHQ();
     EmergencyFixture()
     {
         HQ->AddToInventory(HQ->getStartInventory(StartWares::VLow), true);
@@ -30,21 +30,21 @@ struct EmergencyFixture : public WorldFixture<CreateEmptyWorld, 1>
         world.SetBuildingSite(BuildingType::Farm, pos, 0);
         world.BuildRoad(0, false, world.GetNeighbour(pos, Direction::SouthEast),
                         std::vector<Direction>(3, Direction::West));
-        
+
         pos = world.GetPlayer(0).GetHQPos() + MapPoint(-3, 0);
         world.SetBuildingSite(BuildingType::Farm, pos, 0);
         world.BuildRoad(0, false, world.GetNeighbour(pos, Direction::SouthEast),
                         std::vector<Direction>(3, Direction::East));
 
-        //wait until emergency protocol should be activated
-        RTTR_EXEC_TILL(500,HQ->GetInventory()[GoodType::Boards] == 10);
+        // wait until emergency protocol should be activated
+        RTTR_EXEC_TILL(500, HQ->GetInventory()[GoodType::Boards] == 10);
 
-        //activate program (with 10 boards it should trigger)
+        // activate program (with 10 boards it should trigger)
         world.GetPlayer(0).TestForEmergencyProgramm();
 
-        //wait for some more ticks to give time if not working to deliver more boards
+        // wait for some more ticks to give time if not working to deliver more boards
         RTTR_SKIP_GFS(200);
-        //check boards are still fine and protocol working
+        // check boards are still fine and protocol working
         BOOST_TEST_CHECK(world.GetPlayer(0).GetHQ()->GetInventory()[GoodType::Boards] == 10);
     }
 };
@@ -63,12 +63,12 @@ BOOST_FIXTURE_TEST_CASE(EmergencyProtoclActiveWoodcutterAndSawmillCanBuild, Emer
     world.BuildRoad(0, false, world.GetNeighbour(posSawmill, Direction::SouthEast),
                     std::vector<Direction>(2, Direction::NorthEast));
 
-    //check if inventory boards are given out
-    RTTR_EXEC_TILL(200,HQ->GetInventory()[GoodType::Boards] < 10);
+    // check if inventory boards are given out
+    RTTR_EXEC_TILL(200, HQ->GetInventory()[GoodType::Boards] < 10);
 
-    //check if building where found
-    RTTR_EXEC_TILL(2000,world.GetNO(posWoodcutter)->GetType() == NodalObjectType::Building);
-    RTTR_EXEC_TILL(2000,world.GetNO(posSawmill)->GetType() == NodalObjectType::Building);
+    // check if building where found
+    RTTR_EXEC_TILL(2000, world.GetNO(posWoodcutter)->GetType() == NodalObjectType::Building);
+    RTTR_EXEC_TILL(2000, world.GetNO(posSawmill)->GetType() == NodalObjectType::Building);
 }
 
 BOOST_FIXTURE_TEST_CASE(EmergencyProtoclActiveOtherBuldingsnotBuild, EmergencyFixture)
@@ -80,8 +80,8 @@ BOOST_FIXTURE_TEST_CASE(EmergencyProtoclActiveOtherBuldingsnotBuild, EmergencyFi
     world.BuildRoad(0, false, world.GetNeighbour(pos, Direction::SouthEast),
                     std::vector<Direction>(2, Direction::NorthEast));
 
-    //wait for some more ticks to give time if not working to deliver more boards
+    // wait for some more ticks to give time if not working to deliver more boards
     RTTR_SKIP_GFS(500);
-    //check boards are still fine and protocol working
+    // check boards are still fine and protocol working
     BOOST_TEST_CHECK(world.GetPlayer(0).GetHQ()->GetInventory()[GoodType::Boards] == 10);
 }
